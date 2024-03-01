21072fe93a9c8610955f65abad1a17231377c2aa
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-03-01 20:33:03 +0200

    Updated the articles in the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -47,47 +47,30 @@ const Home = () => {
         <div className="container">
         <h4>Heavy Local Reviews</h4>
         <div className="row mt-4">
-            <div className="col-md-4">
+            <div className="col-md-6">
                 <div className="card p-4">
-                <img className="card-img-top shadow-lg " src={SacrificeInFire}></img>
-                <h4 className="card-title">Sacrifice In Fire:Review</h4>
+                <img className="card-img-top shadow-lg" alt={"Adrastial"} src={"https://heavy-local.com/assets/adrastia1.jpg"}></img>
+                <h4 className="card-title">Απελευθερώνοντας το Ελληνικό μένος: “Αδράστεια” (Album Review)</h4>
                     <div className="card-body text-center">
                         <p className="lead">
-                        Οι Sacrifice in Fire είναι μια death metal μπάντα από τη Γερμανία που ανακάλυψα εντελώς τυχαία μέσω του Instagram. Μπορώ να πω πως πρόκειται για ένα από τα ακατέργαστα διαμάντια που έχω ανακαλύψει. Με μια ματιά στη μουσική τους, αναδεικνύεται ένας δυστοπικός στίχος με την συνοδεία απόκοσμων πλήκτρων, ενώ τα ντραμς με τις κιθάρες προσδίδουν ένα ορμητικό ύφος. Κομμάτια όπως το Concrete Grave και το Weapons of Mass Destruction μας μιλούν για το σκληρό πρόσωπο της ανθρωπότητας και για το τι είναι ικανός να κάνει ο άνθρωπος.
-
-
+                            Η Ελληνική μέταλ σκηνή στέκεται ως μια αξιόλογη δύναμη, όχι μόνο ποσοτικά αλλά και ποιοτικά κυρίως. Ενώ μπάντες όπως οι Thy Darkened Shade και οι Septicflesh κυριαρχούν στις συζητήσεις, η διάσταση της Ελληνικής μέταλ...
                         </p>
-                       <a href="/article/Sacrifice-In-Fire-archive" className="btn btn-primary">Read More</a>
+                       <a href="/article/kawir_archive" className="btn btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
-            <div className="col-md-4">
+            <div className="col-md-6">
             <div className="card p-4">
-                <img className=" card-img-top shadow-lg" src={BluesCondors}></img>
-                <h4 className="card-title"> Blues Condors - 45 Blues:Review</h4>
+                <img className=" card-img-top shadow-lg" src={"https://heavy-local.com/assets/automn.jpeg"}></img>
+                <h4 className="card-title"> Blood, Thunder, and the Roar of Italian Doom: “What's Done is Done” Album Review</h4>
                     <div className="card-body text-center">
                         <p className="lead">
-                        Οι Blues Condors είναι μια μπάντα blues/rock από τη Θεσσαλονίκη. Πριν από δύο χρόνια κυκλοφόρησαν το πρώτο τους EP με τίτλο '45 Blues', το οποίο είχα την ευκαιρία να αποκτήσω σε μορφή hard copy από τον Vapa (μέλος των Blues Condors και των Lioneye).Καθώς εισέρχεται το EP, από τα πρώτα δευτερόλεπτα ακούμε μια παλιά αισθητική σχεδόν από τη δεκαετία του '70. Ο τρόπος με τον οποίο ο τραγουδιστής ερμηνεύει τους στίχους, συνοδευόμενος από επιρροές από το στιλ του Jim Morrison (The Doors), είναι εμφανής. Οι κιθάρες δεν είναι τόσο βαριές και παραμορφωμένες, διατηρώντας έναν χορευτικό ρυθμό, ενώ ταυτόχρονα δημιουργούν έναν μυστηριώδη αέρα.
+                            If you want blood, if you want uncontained rage, and if on top of that you want to see aggression of sound succeed, then in the period of autumn’s splendor shall you discover it...
                         </p>
-                       <a href="/article/Blues-Condors-archive" className="btn btn-primary">Read More</a>
+                       <a href="/article/whats_done_is_done" className="btn btn-primary">Read More</a>
                     </div>
-                </div>
-
             </div>
-            <div className="col-md-4">
-            <div className="card text-center">
-            <img className=" card-img-top shadow-lg" src={ADE}></img>
-                <h4 className="card-title">Veins of Darkness: Udåd Emerges with Raw Norwegian Black Metal</h4>
-                    <div className="card-body text-center">
-                        <p className="lead">
-                        Udåd emerges from the shadowy depths, delivering a primal offering steeped in the essence of Norwegian Black Metal. Spearheaded by Thomas Eriksen, the mastermind behind Mork, this debut release ignites a blazing trail back to the genre’s raw and uncompromising origins. Udåd stands as a stark departure from the intricate tapestries woven by Mork, instead embracing a primal ethos reminiscent of the early 1990s Black Metal movement. Drawing inspiration from the bone-chilling atmospheres </p>
-                       <a href="/article/Udad-archive" className="btn btn-primary">Read More</a>
-
-                    </div>
-
             </div>
-
-          </div>
         <hr className="bg-dark" />
 
         </div>
