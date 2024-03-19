6ca18bbfadf0482b751f369b5a7f0c0834c854f1
Vaggelis Tzimas <111343674+Vaggtzm@users.noreply.github.com>
2024-03-19 19:38:20 +0200

    update 19/24

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Navigation from "../compoments/Navigation/Navigation";
 import "./home.css";
-import WIckedWizard from './../assets/IdiotsCorrected.webp';
-import Hexaban from './../assets/Hexaban.webp';
-import Demande from './../assets/Demande.webp';
+import Mnima from './../assets/Mnima.jpg';
+import Kroheim from './../assets/kroheim.jpg';
+import Passengers from './../assets/passengers.jpeg';
 import spell from './../assets/LiveSpell.jpg';
 import Yothiria from "./../assets/chronicles/chronicle-02/yothiria-02.jpg";
 import Contact from "../compoments/ContactForm/contact";
@@ -50,44 +50,38 @@ const Home = () => {
   <div className="row mt-4 text-center">
     <div className="col-md-4 mb-4">
       <div className="card h-100 w-100">
-        <img className="card-img-top shadow-lg img-fluid" src={WIckedWizard} alt="Dreariness"></img>
+        <img className="card-img-top shadow-lg img-fluid" src={Mnima} alt="Dreariness"></img>
         <div className="card-body">
-          <h4 className="card-title">Wicked Wizzard - Warlords of the Dark Realm (Album Review)</h4>
+          <h4 className="card-title">Mnima :Band Review</h4>
           <p className="card-text lead">
-          Οι Ισπανοί Wicked Wizzard είναι μια μπάντα η οποία αντιπροσωπεύει τον αγνό ήχο του stoner των 70s όπως “πρωτοπλάστηκε” από τους αξεπέραστους Black Sabbath. Η τριάδα των Iñigo Jauregui, Unai Minguez και Mikel Bidaurrazaga, ιδρυμένοι το 2017 και ορμώμενοι από την Mungia της Βασκονίας, το 2020 συνέθεσε ένα άλμπουμ το οποίο μπορώ να ακούω προσωπικά με τις ώρες και το συστήνω ανεπιφύλακτα σε όλους τους αναγνώστες μας.
+          Είναι γεγονός πως μετά από δύο χρόνια από το τελευταίο τους full-length, οι Έλληνες noise black metallers Μνήμα (Mnima) επέστρεψαν με μια πολύ δυνατή νέα κυκλοφορία αποτελούμενη από 4 αργά και βασανιστικά κομμάτια. Τα "Καταραμένα Λείψανα" ήρθαν υποσχόμενα να σηκώσουν τις τρίχες στους σβέρκους μας, με τα απόμακρα, θλιμμένα riffs, τον μόνιμο ήχο των ραδιοφωνικών παρασίτων και τα απόκοσμα ουρλιαχτά γεμάτα οδύνη.
 
-Τεχνικά οι Wicked Wizzard είναι πιστοί στις stoner αρχές, το μπάσο είναι το Α και το Ω, βαριά distortions με δυναμικά groovy riffs και clean vocals, ενώ τηρούν την προαναφερθείσα αγνότητα της πρωταρχικής stoner μορφής επιτυγχάνοντας στο να δημιουργήσουν έναν υπερβατικό και δυναμικό ήχο με σαφή δομή και μια εξαίσια σκοτεινή ατμοσφαιρικότητα, φουντώνοντας την φαντασία μας και φέρνοντάς μας στην πολυπόθητη έκσταση!          </p>
-          <a href="/article/Wicked_wizzard-archive" className="btn btn-primary">Read More</a>
+Ένα ακόμα αφιέρωμα στους τρόμους και τον μυστικισμό που κρύβει η τελετή της κηδείας, ο πόνος του θανάτου και η ματαιότητα του θνητού κόσμου του ανθρώπου. Εκεί που όλα τελειώνουν εκτός από τη μιζέρια, την αποσύνθεση και την παράνοια. Στο cover που στολίζει το συγκεκριμένο άλμπουμ βλέπουμε δύο χρώματα, μόνο το έντονο κόκκινο μέσα σε κατάμαυρο φόντο. Στα δεξιά του πλαισίου παριστάνεται ο εσταυρωμένος σε μια κατάσταση που προκαλεί απελπισία στα μάτια του θεατή. Μα αυτό που πραγματικά προκαλεί απόγνωση στη ψυχή είναι ο ήχος που συνοδεύεται από το album cover.  </p>
+          <a href="/article/mnima-archive" className="btn btn-primary">Read More</a>
         </div>
       </div>
     </div>
     <div className="col-md-4 mb-4">
       <div className="card h-100 w-100">
-        <img className="card-img-top shadow-lg img-fluid" src={Hexaban} alt="Zong"></img>
+        <img className="card-img-top shadow-lg img-fluid" src={Kroheim} alt="Zong"></img>
         <div className="card-body">
-          <h4 className="card-title">Spellbinding Soundscapes: “As the Night Approaches”Album Review(ΕNG)</h4>
+          <h4 className="card-title">Kromheim - Band review</h4>
           <p className="card-text lead">
-          Prepare to be engulfed by the shadows as Hexenbann unleashes their sonic onslaught “As the Night Approaches”! From the explosive opening track to the haunting finale, the album is a masterclass in modern Metal, highlighting the band members’ skill and unwavering commitment to their artistry.
-Embracing the Night: Hexenbann's Evocative Ode to Darkness
-
-Hexenbann wastes no time in grabbing your attention with the spellbinding album. From the moment the first note hits, you're thrust into a whirlwind of frenetic energy and unrelenting aggression. The blistering guitar riffs and thunderous drums set the stage for an auditory assault that refuses to let up. With its relentless intensity and haunting atmosphere, “As the Night Approaches” serves as the perfect introduction to the dark and mysterious world of Hexenbann. The album delves into realms of mysticism, painting vivid scenes of nature's majesty and exploring the darkest recesses of human existence. Through enchanting narratives and thought-provoking introspection, the band navigates themes of life's complexities, juxtaposing dark, dissonant atmospheres with moments of luminous optimism. Much like the cycle of day and night, the album’s message resonates—where light and warmth flourish by day, only to fade into the cold embrace of darkness as dusk falls.
+          Οι Kromheim ξεκίνησαν ως ένα one - man project το 2020 στην Πολωνία, μια χώρα με αξιέπαινη παρουσία στον χώρο της extreme metal, με μπάντες όπως οι Vader και οι Behemoth να έχουν αναδειχθεί και να έχουν αφήσει ανεξίτηλο το σημάδι τους παγκοσμίως. Ιδρυτής τους ήταν ο Mikołaj Popławski (Μικολάι Ποπλιάβσκι) ο οποίος αποφάσισε το 2022 να μετακινηθεί στην Αγγλία όπου θα μετέτρεπε τους Kromheim σε μπάντα με κανονική σύνθεση, στο πλήρωμα από το 2022 θα συμμετείχαν εφεξής οι Duncan Arkley (μπάσο)  και Sot (κιθάρες). Το πρώτο τους έργο θα ήταν το 2020 το EP με όνομα Kromheim, στο οποίο ο Ποπλιάβσκι τα είχε αναλάβει εξ ολοκλήρου όλα μόνος του συνθετικά και ορχηστρικά, το 2022 με την συμμετοχή των Arkley και Sot πλέον θα κυκλοφορούσαν ένα single το The Landing ενώ το 2023 σε μια πλήρως παραγωγική χρονιά θα έβγαζαν στην δημοσιότητα 2 singles (War/ World Reborn) και το πρώτο τους πλήρες album το Journey To Divinity.
           </p>
-          <a href="/article/Hexaban-archive" className="btn btn-primary">Read More</a>
+          <a href="/article/Kromheim_-_Band_review-archive" className="btn btn-primary">Read More</a>
         </div>
       </div>
     </div>
     <div className="col-md-4 mb-4">
       <div className="card h-100 w-100">
-        <img className="card-img-top shadow-lg img-fluid" src={Demande} alt="Disimulator"></img>
+        <img className="card-img-top shadow-lg img-fluid" src={Passengers} alt="Disimulator"></img>
         <div className="card-body">
-          <h4 className="card-title">The Depths of Human Torment: Demande à la Poussière Presents “Kintsugi” (ENG)</h4>
+          <h4 className="card-title">Chaos and Charms: “Passengers in Panic”</h4>
           <p className="card-text lead">
-          In a melodic cascade that echoes through the shadows, Demande à la Poussière introduces “Kintsugi”, providing a window into the haunting depths of the forthcoming album. The song masterfully blends elements of Black Metal, Doom, and Sludge, crafting a narrative steeped in anguish and introspection.
-Of Darkness, Wounds and Symphonies
-
-“Kintsugi” draws the listeners into a realm cloaked in darkness, where apocalyptic atmospheres and anguished voices hold sway. It unravels like a sinister tableau, each note carving deeper into the psyche, trailing haunting melodies in its wake.Despite its bleak landscape, “Kintsugi” is not devoid of light. Amidst the gloomy sludge scenery, melodic sparks emerge, piercing through the darkness like distant stars in an endless night. These fleeting moments of brightness add depth and dimension to the sonic tapestry woven by Demande à la Poussière. With “Kintsugi”, Demande à la Poussière invites the listeners on an introspective journey. Delving into the wounds of the human condition, the song serves as a mirror reflecting the depths of despair and melancholy. Through their music, the Parisian ensemble navigates the complexities of existence with raw honesty and unflinching authenticity.
+          Οι Passengers in Panic, μια εξ Αθηνών Progressive/Folk metal μπάντα, είναι μια ξεχωριστή δύναμη στην Ελληνική underground σκηνή. Αντλώντας στοιχεία από το ευρύ φάσμα της κλασικής μέταλ, τις μελωδίες της παραδοσιακής Ελληνικής μουσική και της πανκοειδούς (sic) hard rock, ο ετερόκλητος ήχος τους αιχμαλωτίζει τους ακροατές από την πρώτη κιόλας συγχορδία.
           </p>
-          <a href="/article/Demande_a_la_poussiere-archive" className="btn btn-primary">Read More</a>
+          <a href="/article/Chaos_and_Charm-archive" className="btn btn-primary">Read More</a>
         </div>
       </div>
     </div>
