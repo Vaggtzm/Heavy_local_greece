7216b1c707613d7dfd78e0e876ba6cd4f2ab74cc
Vaggelis Tzimas <111343674+Vaggtzm@users.noreply.github.com>
2024-03-21 13:17:39 +0200

    Update articles.js

--- src/pages/Articles/articles.js
+++ src/pages/Articles/articles.js
@@ -8,6 +8,9 @@ import TheyCameFromVissions from './../../assets/They_Came_from_Visions_Album.jp
 import Venus from './../../assets/Venus.jpg';
 import Falooda from './../../assets/Falooda_Demo_2024_artwork.jpg';
 import InnerLoad from './../../assets/InnerLord.webp';
+import WickedWizard from './../../assets/IdiotsCorrected.webp';
+import Hexaban from './../../assets/Hexaban.webp';
+import Demande from './../../assets/Demande.webp';
 import "./../home.css"
 const Articles =()=>{
     return(
@@ -19,6 +22,45 @@ const Articles =()=>{
             </h5>
             <hr className="bg-dark"></hr>
             <div className="row text-center">
+            <p className="lead">13.03.24</p>
+            <div className="col-md-6">
+            <h4>Spellbinding Soundscapes: “As the Night Approaches”Album Review(ΕNG)</h4>
+                <img src={Demande} className="w-100 h-75"></img>
+            </div>
+            <div className="col-md-6">
+            <h3>By Daria</h3>
+                <p className="lead">
+                In a melodic cascade that echoes through the shadows, Demande à la Poussière introduces “Kintsugi”, providing a window into the haunting depths of the forthcoming album. The song masterfully blends elements of Black Metal, Doom, and Sludge, crafting a narrative steeped in anguish and introspection. Of Darkness, Wounds and Symphonies “Kintsugi” draws the listeners into a realm cloaked in darkness, where apocalyptic atmospheres and anguished voices hold sway. It unravels like a sinister tableau, each note carving deeper into the psyche, trailing haunting melodies in its wake.Despite its bleak landscape, “Kintsugi” is not devoid of light. Amidst the gloomy sludge scenery, melodic sparks emerge, piercing through the darkness like distant stars in an endless night. These fleeting moments of brightness add depth and dimension to the sonic tapestry woven by Demande à la Poussière. With “Kintsugi”, Demande à la Poussière invites the listeners on an introspective journey. Delving into the wounds of the human condition, the song serves as a mirror reflecting the depths of despair and melancholy. Through their music, the Parisian ensemble navigates the complexities of existence with raw honesty and unflinching authenticity.
+                </p>
+                <a href="/article/Demande_a_la_poussiere-archive" className="btn btn-danger">Read More</a>
+
+            </div>
+            <div className="col-md-6">
+            <h4>Spellbinding Soundscapes: “As the Night Approaches”Album Review(ΕNG)</h4>
+                <img src={Hexaban} className="w-100 h-75"></img>
+            </div>
+            <div className="col-md-6">
+            <h3>By Daria</h3>
+            <p className="lead">
+            Prepare to be engulfed by the shadows as Hexenbann unleashes their sonic onslaught “As the Night Approaches”! From the explosive opening track to the haunting finale, the album is a masterclass in modern Metal, highlighting the band members’ skill and unwavering commitment to their artistry. Embracing the Night: Hexenbann's Evocative Ode to Darkness Hexenbann wastes no time in grabbing your attention with the spellbinding album. From the moment the first note hits, you're thrust into a whirlwind of frenetic energy and unrelenting aggression. The blistering guitar riffs and thunderous drums set the stage for an auditory assault that refuses to let up. With its relentless intensity and haunting atmosphere, “As the Night Approaches” serves as the perfect introduction to the dark and mysterious world of Hexenbann. The album delves into realms of mysticism, painting vivid scenes of nature's majesty and exploring the darkest recesses of human existence. Through enchanting narratives and thought-provoking introspection, the band navigates themes of life's complexities, juxtaposing dark, dissonant atmospheres with moments of luminous optimism. Much like the cycle of day and night, the album’s message resonates—where light and warmth flourish by day, only to fade into the cold embrace of darkness as dusk falls.
+            </p>
+            <a href="/article/Hexaban-archive" className="btn btn-danger">Read More</a>
+            </div>
+
+
+            
+            <div className="col-md-6">
+            <h4>Wicked Wizzard - Warlords of the Dark Realm (Album Review)</h4>
+                <img src={WickedWizard} className="w-100 h-75"></img>
+            </div>
+            
+            <div className="col-md-6">
+            <h3>Απο τον Μιχαλη</h3>
+            <p className="lead">
+            Οι Ισπανοί Wicked Wizzard είναι μια μπάντα η οποία αντιπροσωπεύει τον αγνό ήχο του stoner των 70s όπως “πρωτοπλάστηκε” από τους αξεπέραστους Black Sabbath. Η τριάδα των Iñigo Jauregui, Unai Minguez και Mikel Bidaurrazaga, ιδρυμένοι το 2017 και ορμώμενοι από την Mungia της Βασκονίας, το 2020 συνέθεσε ένα άλμπουμ το οποίο μπορώ να ακούω προσωπικά με τις ώρες και το συστήνω ανεπιφύλακτα σε όλους τους αναγνώστες μας. Τεχνικά οι Wicked Wizzard είναι πιστοί στις stoner αρχές, το μπάσο είναι το Α και το Ω, βαριά distortions με δυναμικά groovy riffs και clean vocals, ενώ τηρούν την προαναφερθείσα αγνότητα της πρωταρχικής stoner μορφής επιτυγχάνοντας στο να δημιουργήσουν έναν υπερβατικό και δυναμικό ήχο με σαφή δομή και μια εξαίσια σκοτεινή ατμοσφαιρικότητα, φουντώνοντας την φαντασία μας και φέρνοντάς μας στην πολυπόθητη έκσταση! 
+            </p>
+            <a href="/article/Wicked_wizzard-archive" className="btn btn-danger">Read More</a>
+            </div>
             <p className="lead">11.03.24</p>
               <div className="col-md-6">
               <h4>Venus:Obscured Until Observed-Album Review</h4>
