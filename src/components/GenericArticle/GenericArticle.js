15daf0cbe63377ea89d66df84022499e7495631a
Vaggelis Tzimas <111343674+Vaggtzm@users.noreply.github.com>
2024-05-09 18:36:38 +0300

    url title and img save funtion (not workint)

--- src/components/GenericArticle/GenericArticle.js
+++ src/components/GenericArticle/GenericArticle.js
@@ -6,138 +6,181 @@ import { ref as databaseRef, push, remove, onValue } from "firebase/database";
 import SocialBar from "../ShareBtns/SocialMediaBar";
 import PageWithComments from "../Comments/comment";
 import ReadMore from "../ReadMore/ReadMore";
-import {Button} from "react-bootstrap";
-import AppNav from './../AppNav/AppNav'
+import { Button } from "react-bootstrap";
+import AppNav from "./../AppNav/AppNav";
 
 const DefaultArticle = (props) => {
-    const [isEarlyAccess] = useState(props.earlyAccess);
-    const { name } = useParams();
-    const [articles, setArticles] = useState({});
-    const [enableSaving, setEnableSaving] = useState(false);
-    const [isSaved, setIsSaved] = useState(false); // State to track if the article is saved by the user
+  const [isEarlyAccess] = useState(props.earlyAccess);
+  const { name } = useParams();
+  const [articles, setArticles] = useState({});
+  const [enableSaving, setEnableSaving] = useState(false);
+  const [isSaved, setIsSaved] = useState(false); // State to track if the article is saved by the user
 
-    useEffect(() => {
-        fetchData().then(); // Fetch article data when component mounts
+  useEffect(() => {
+    fetchData().then(); // Fetch article data when component mounts
 
-        // Check if user is authenticated
-        auth.onAuthStateChanged((user) => {
-            if (user) {
-                setEnableSaving(true); // Enable saving functionality if user is logged in
-            }
+    // Check if user is authenticated
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setEnableSaving(true); // Enable saving functionality if user is logged in
+      }
+    });
+
+    // Fetch user's saved articles from Firebase Realtime Database
+    const fetchSavedStatus = async () => {
+      const currentUser = auth.currentUser;
+      if (currentUser) {
+        const savedArticlesRef = databaseRef(
+          database,
+          `users/${currentUser.uid}/savedArticles`
+        );
+        onValue(savedArticlesRef, (snapshot) => {
+          const savedArticles = snapshot.val() || {};
+          setIsSaved(savedArticles[name]); // Check if current article is in user's saved list
         });
-
-        // Fetch user's saved articles from Firebase Realtime Database
-        const fetchSavedStatus = async () => {
-            const currentUser = auth.currentUser;
-            if (currentUser) {
-                const savedArticlesRef = databaseRef(database, `users/${currentUser.uid}/savedArticles`);
-                onValue(savedArticlesRef, (snapshot) => {
-                    const savedArticles = snapshot.val() || {};
-                    setIsSaved(savedArticles[name]); // Check if current article is in user's saved list
-                });
-            }
-        };
-
-        fetchSavedStatus(); // Fetch saved status when component mounts
-
-        return () => {
-            const currentUser = auth.currentUser;
-            if (currentUser) {
-                const savedArticlesRef = databaseRef(database, `users/${currentUser.uid}/savedArticles`);
-
-            }
-        };
-    }, [name]);
-
-    const fetchData = async () => {
-        try {
-            let articleSnapshot;
-            if (isEarlyAccess) {
-                articleSnapshot = await getDownloadURL(ref(storage, `early_releases/${name}.json`));
-            } else {
-                articleSnapshot = await getDownloadURL(ref(storage, `articles/${name}.json`));
-            }
-
-            const response = await fetch(articleSnapshot);
-            if (!response.ok) {
-                throw new Error("Failed to fetch data");
-            }
-            const data = await response.json();
-            data.img01 = await getFirebaseStorageUrl(data.img01);
-            setArticles(data);
-        } catch (error) {
-            console.error("Error fetching data:", error.message);
-        }
+      }
     };
 
-    const getFirebaseStorageUrl = async (imageUrl) => {
-        const fileName = imageUrl.split("/").pop();
-        const storageRef = ref(storage, `images/${fileName}`);
-        return await getDownloadURL(storageRef);
-    };
+    fetchSavedStatus(); // Fetch saved status when component mounts
 
-    const toggleSaveArticle = () => {
-        const currentUser = auth.currentUser;
-        if (currentUser) {
-            const savedArticlesRef = databaseRef(database, `users/${currentUser.uid}/savedArticles/${name}`);
-            if (isSaved) {
-                remove(savedArticlesRef); // Remove article from user's saved list
-                setIsSaved(false);
-            } else {
-                push(savedArticlesRef, true); // Add article to user's saved list
-                setIsSaved(true);
-            }
-        }
+    return () => {
+      const currentUser = auth.currentUser;
+      if (currentUser) {
+        const savedArticlesRef = databaseRef(
+          database,
+          `users/${currentUser.uid}/savedArticles`
+        );
+      }
     };
+  }, [name]);
 
-    if (!articles || Object.keys(articles).length === 0) {
-        return <p>Loading...</p>;
+  const fetchData = async () => {
+    try {
+      let articleSnapshot;
+      if (isEarlyAccess) {
+        articleSnapshot = await getDownloadURL(
+          ref(storage, `early_releases/${name}.json`)
+        );
+      } else {
+        articleSnapshot = await getDownloadURL(
+          ref(storage, `articles/${name}.json`)
+        );
+      }
+
+      const response = await fetch(articleSnapshot);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json();
+      data.img01 = await getFirebaseStorageUrl(data.img01);
+      setArticles(data);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
     }
+  };
 
-    return (
-        <>
-            {/* Meta tags component */}
-            <AppNav />
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12 d-flex justify-content-evenly">
-                        <h3>{articles.title}</h3>
-                        <hr className="bg-dark" />
-                    </div>
-                    <p className="lead">{articles.sub}</p>
-                    <hr className="bg-dark" />
-                    <div className="col-md-6 credits-box">
-                        <img className="img-fluid w-100" src={articles.img01} alt={articles.title}/>
-                        <p className="lead">
-                            <span dangerouslySetInnerHTML={{__html: articles.details}}></span>
-                        </p>
-                        <p className="lead">
-                            <span dangerouslySetInnerHTML={{__html: articles.Socials}}></span>
-                        </p>
-                        {/*TODO: fix styling*/}
-                        <hr className="bg-dark" />
-                        {enableSaving && (
-                            <span className="btn btn-danger w-25  rounded-4" onClick={toggleSaveArticle} style={{ cursor: 'pointer'}}>
-                            save 
-                                {isSaved ? (
-                                    <i className="fas fa-heart" style={{ color: 'red' }}></i>
-                                ) : (
-                                    <i className="far fa-heart"></i>
-                                )}
-                            </span>
-                        )}
-                        <a href="https://buymeacoffee.com/tzimasvagg7"  className="btn btn-danger 25 m-2  rounded-4" >Say Thanks</a>
-                    </div>
-                    <div className="col-md-6">
-                        <div dangerouslySetInnerHTML={{__html: articles.content}}></div>
-                        <SocialBar/>
-                        <PageWithComments/>
-                    </div>
-                    <ReadMore/>
-                </div>
-            </div>
-        </>
-    );
+  const getFirebaseStorageUrl = async (imageUrl) => {
+    const fileName = imageUrl.split("/").pop();
+    const storageRef = ref(storage, `images/${fileName}`);
+    return await getDownloadURL(storageRef);
+  };
+
+  const toggleSaveArticle = (title, img01, url) => {
+    const currentUser = auth.currentUser;
+    if (currentUser) {
+      const savedArticlesRef = databaseRef(
+        database,
+        `users/${currentUser.uid}/savedArticles/${name}`
+      );
+      if (isSaved) {
+        remove(savedArticlesRef); // Remove article from user's saved list
+        setIsSaved(false);
+      } else {
+        // Check if title, img01, and url are not empty
+        if (title && img01 && url) {
+          push(savedArticlesRef, {
+            title,
+            img01,
+            url,
+          }); // Add article to user's saved list
+          setIsSaved(true);
+        } else {
+          console.log("Title, image URL, or article URL is missing");
+        }
+      }
+    }
+  };
+  if (!articles || Object.keys(articles).length === 0) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      {/* Meta tags component */}
+      <AppNav />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 d-flex justify-content-evenly">
+            <h3>{articles.title}</h3>
+            <hr className="bg-dark" />
+          </div>
+          <p className="lead">{articles.sub}</p>
+          <hr className="bg-dark" />
+          <div className="col-md-6 credits-box">
+            <img
+              className="img-fluid w-100"
+              src={articles.img01}
+              alt={articles.title}
+            />
+            <p className="lead">
+              <span
+                dangerouslySetInnerHTML={{ __html: articles.details }}
+              ></span>
+            </p>
+            <p className="lead">
+              <span
+                dangerouslySetInnerHTML={{ __html: articles.Socials }}
+              ></span>
+            </p>
+            {/*TODO: fix styling*/}
+            <hr className="bg-dark" />
+            {enableSaving && (
+              <span
+                className="btn btn-danger w-25 rounded-4"
+                onClick={() =>
+                  toggleSaveArticle(
+                    articles.title,
+                    articles.img01,
+                    articles.url
+                  )
+                }
+                style={{ cursor: "pointer" }}
+              >
+                save
+                {isSaved ? (
+                  <i className="fas fa-heart" style={{ color: "red" }}></i>
+                ) : (
+                  <i className="far fa-heart"></i>
+                )}
+              </span>
+            )}{" "}
+            <a
+              href="https://buymeacoffee.com/tzimasvagg7"
+              className="btn btn-danger 25 m-2  rounded-4"
+            >
+              Say Thanks
+            </a>
+          </div>
+          <div className="col-md-6">
+            <div dangerouslySetInnerHTML={{ __html: articles.content }}></div>
+            <SocialBar />
+            <PageWithComments />
+          </div>
+          <ReadMore />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DefaultArticle;
--- src/components/Users/Pages/Saved.js
+++ src/components/Users/Pages/Saved.js
@@ -1,53 +1,64 @@
-import React , {useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { database } from "../../../firebase";
-import { ref,onValue } from "firebase/database";
+import { ref, onValue } from "firebase/database";
 import AppNavigation from "../../AppNav/AppNav";
 import { auth } from "../../../firebase";
 import { useParams } from "react-router-dom";
 
-const SavedArtciles = () => {
+const SavedArticles = () => {
   const [data, setData] = useState(null);
-  const [ currentUser,setCurrentUser] = useState();
+  const [currentUser, setCurrentUser] = useState();
 
-  
-  auth.onAuthStateChanged( async (user) =>{
-    if (user) {
-      setCurrentUser(user)
-      console.log(user , "User logged in ");
-        const dataRef = ref(database, `users/${user.uid}/savedArticles/`);
-        onValue(dataRef, (snapshot) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      auth.onAuthStateChanged(async (user) => {
+        if (user) {
+          setCurrentUser(user);
+          const dataRef = ref(database, `users/${user.uid}/savedArticles/`);
+          onValue(dataRef, (snapshot) => {
             const fetchedData = snapshot.val();
-            console.log(fetchedData , "Fetched data");
             setData(fetchedData);
+            console.log(fetchedData ,"data")
+          });
+        }
+      });
+    };
+    fetchData();
+  }, []);
 
-        });
-    } else {
-        console.log("Ο χρήστης δεν είναι συνδεδεμένος");
-    }
-});
-    return(
-        <>
-        <AppNavigation />
-        <div className="container">
-        <div className="row">
-          {data ? (
-            Object.keys(data).map((key) => (
-              <div className="col-md-4" key={key}>
-                <div className="card mb-3">
+  return (
+    <>
+   <AppNavigation />
+    <div className="container">
+      <h1>Saved Articles</h1>
+      <hr className="bg-dark" />
+      <div className="row">
+        {data ? (
+          Object.entries(data).map(([key, isSaved]) => (
+            <div className="col-md-4 mb-4" key={key}>
+              <div className="card">
+                {isSaved ? (
+                  
                   <div className="card-body">
-                    <h5 className="card-title">{data[key].title}</h5>
-                    <p className="card-text">{data[key].description}</p>
+                    <h5 className="card-title">{key}</h5>
+                    <p className="card-text">This article is saved</p>
                   </div>
-                </div>
+                ) : (
+                  <div className="card-body">
+                    <h5 className="card-title">{key}</h5>
+                    <p className="card-text">This article is not saved</p>
+                  </div>
+                )}
               </div>
-            ))
-          ) : (
-            <p>Loading...</p>
-          )}
-        </div>
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
+    </div>
+    </>
+  );
+};
 
-        </>
-    )
-}
-export default SavedArtciles;
+export default SavedArticles;
