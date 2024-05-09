d711b2dc7ca4c90aff71ae8cab9439219409813e
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-05-09 22:39:11 +0300

    fixed bug

--- src/components/Users/Pages/Saved.js
+++ src/components/Users/Pages/Saved.js
@@ -12,56 +12,24 @@ const SavedArtciles = () => {
   const [data, setData] = useState(null);
   const [ currentUser,setCurrentUser] = useState();
 
-  
-  auth.onAuthStateChanged( async (user) =>{
-    if (user) {
-      setCurrentUser(user)
-      console.log(user , "User logged in ");
-        const dataRef = databaseRef(database, `users/${user.uid}/savedArticles/`);
-        onValue(dataRef, (snapshot) => {
-            const fetchedData = snapshot.val();
-            console.log(fetchedData , "Fetched data");
-            setData(fetchedData);
+  useEffect(() => {
+      auth.onAuthStateChanged( async (user) =>{
+          if (user) {
+              setCurrentUser(user)
+              console.log(user , "User logged in ");
+              const dataRef = databaseRef(database, `users/${user.uid}/savedArticles/`);
+              onValue(dataRef, (snapshot) => {
+                  const fetchedData = snapshot.val();
+                  console.log(fetchedData , "Fetched data");
+                  setData(fetchedData);
 
-        });
-    } else {
-        console.log("Ο χρήστης δεν είναι συνδεδεμένος");
-    }
-});
+              });
+          } else {
+              console.log("Ο χρήστης δεν είναι συνδεδεμένος");
+          }
+      });
+  },[])
 
-const getFirebaseStorageUrl = async (imageUrl) => {
-  const fileName = imageUrl.split("/").pop();
-  const storageRef = ref(storage, `images/${fileName}`);
-  return await getDownloadURL(storageRef);
-};
-
-
-const fetchData = async (link) => {
-  const isEarlyAccess = false;
-
-  try {
-    let articleSnapshot;
-    if (isEarlyAccess) {
-      articleSnapshot = await getDownloadURL(
-        ref(storage, `early_releases/${link}.json`)
-      );
-    } else {
-      articleSnapshot = await getDownloadURL(
-        ref(storage, `articles/${link}.json`)
-      );
-    }
-
-    const response = await fetch(articleSnapshot);
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const data = await response.json();
-    data.img01 = await getFirebaseStorageUrl(data.img01);
-    return  data
-  } catch (error) {
-    console.error("Error fetching data:", error.message);
-  }
-};
 
     return(
         <>
@@ -70,14 +38,14 @@ const fetchData = async (link) => {
       <h1>Saved Articles</h1>
       <div className="row">
         {data ? (
-          Object.entries(data).map(([key, isSaved]) => (
-            <div className="col-md-4 mb-4" key={key}>
-            
-              <div className="card">
-              <SavedArticleData data={fetchData(key)} key={key} isSaved={isSaved}/>
-              </div>
-            </div>
-          ))
+            Object.entries(data).map(([articleKey, articleData]) => (
+                <div className="col-md-4 mb-4" key={articleKey}>
+                    <div className="card">
+                        <SavedArticleData article={articleKey} isSaved={articleData}/>
+                    </div>
+                </div>
+            ))
+
         ) : (
           <p>Loading...</p>
         )}
--- src/components/Users/SavedArticleData.js
+++ src/components/Users/SavedArticleData.js
@@ -1,18 +1,62 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {getDownloadURL, ref} from "firebase/storage";
+import {storage} from "../../firebase";
 
 
 
-const SavedArticleData = ({data,isSaved ,key}) =>{
+const SavedArticleData = (props) =>{
+    const [data, setData] = useState({});
+
+    const getFirebaseStorageUrl = async (imageUrl) => {
+        const fileName = imageUrl.split("/").pop();
+        const storageRef = ref(storage, `images/${fileName}`);
+        return await getDownloadURL(storageRef);
+    };
+
+    const fetchData = async (link) => {
+        const isEarlyAccess = false;
+
+        try {
+            let articleSnapshot;
+            if (isEarlyAccess) {
+                articleSnapshot = await getDownloadURL(
+                    ref(storage, `early_releases/${link}.json`)
+                );
+            } else {
+                articleSnapshot = await getDownloadURL(
+                    ref(storage, `articles/${link}.json`)
+                );
+            }
+
+            const response = await fetch(articleSnapshot);
+            if (!response.ok) {
+                throw new Error("Failed to fetch data");
+            }
+            const data = await response.json();
+            data.img01 = await getFirebaseStorageUrl(data.img01);
+            return  data
+        } catch (error) {
+            console.error("Error fetching data:", error.message);
+        }
+    };
+
+    useEffect(() => {
+        console.log(props.article);
+        fetchData(props.article).then((tempData) => {
+            console.log(tempData);
+            setData(tempData);
+        });
+    },[])
     return(
         <>
-            {isSaved ? (
+            {props.isSaved ? (
             <div className="card-body">
-              <h5 className="card-title">{data.title}</h5>
+                  <h5 className="card-title">{data.title}</h5>
               <p className="card-text">This article is saved</p>
             </div>
           ) : (
             <div className="card-body">
-              <h5 className="card-title">{data.title}</h5>
+              <h5 className="card-title">{props.article}</h5>
               <p className="card-text">This article is not saved</p>
             </div>
           )}
