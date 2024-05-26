47b55cb95cf77602963c2a3be4313f6fa91b7c75
Vaggelis Tzimas <111343674+Vaggtzm@users.noreply.github.com>
2024-05-26 21:19:46 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -313,20 +313,20 @@ const Home = () => {
     <h4>Reviews (GR)</h4>
     <div className="col-md-4">
     <div className="card h-100 w-100 review-card">
-        <img className="card-img-top shadow-lg h-100" src={"https://heavy-local.com/assets/korrection.webp"} alt="Disimulator"></img>
+        <img className="card-img-top shadow-lg h-100" src={"https://pulse-of-the-underground.com/assets/a3925169802_10.jpg"} alt="Disimulator"></img>
         <div className="card-body">
-          <h4 className="card-title">Παλαιάς κοπής ανταρσία - KorrectioN - Having Fun (single review + info)</h4>
-          <a href="/article/korrection-archive" className="btn btn-primary">Read More</a>
+          <h4 className="card-title">Το Πιοτό των Μυστών - Khirki - Κυκεώνας (Album Review) </h4>
+          <a href="/article/Το-πιοτό-των-Μυστών---Khirki---Κυκεώνας-(Album-Review)-" className="btn btn-primary">Read More</a>
         </div>
       </div>
     </div>
 
     <div className="col-md-4">
     <div className="card h-100 w-100 review-card">
-        <img className="card-img-top shadow-lg h-100" src={'https://heavy-local.com/assets/Degenerate.jpg'} alt="Disimulator"></img>
+        <img className="card-img-top shadow-lg h-100" src={'https://pulse-of-the-underground.com/assets/Noiz-Ritual-Band-1.jpg'} alt="Disimulator"></img>
         <div className="card-body">
-          <h4 className="card-title">Μια αμφίρροπη μάχη - Degenerate Mind - Nothing Left (single review)</h4>
-          <a href="/article/degenerate-mind-archive" className="btn btn-primary">Read More</a>
+          <h4 className="card-title">Ξετυλίγοντας την ψυχή - Noiz Ritual - Embrace The Noiz (EP review + band info)</h4>
+          <a href="/article/Ξετυλίγοντας-την-ψυχή---Noiz-Ritual---Embrace-The-Noiz-(EP-review-+-band-info)" className="btn btn-primary">Read More</a>
         </div>
       </div>
     </div>
@@ -335,68 +335,29 @@ const Home = () => {
 
     <div className="col-md-4">
     <div className="card h-100 w-100 review-card">
-        <img className="card-img-top shadow-lg  h-100" src={"https://heavy-local.com/assets/Herc.jpg"} alt="Disimulator"></img>
+        <img className="card-img-top shadow-lg  h-100" src={"https://pulse-of-the-underground.com/assets/images.png"} alt="Disimulator"></img>
         <div className="card-body">
           <h4 className="card-title">Βουτιά στην περιπέτεια του Silmarillion- Herc - “Of Light and Darkness” (album review)</h4>
-          <a href="/article/Silmarillion-archive" className="btn btn-primary">Read More</a>
+          <a href="/article/Hard’n’-heavy--Blues---Rattlesquad---1233-(album-review-+-band-info)" className="btn btn-primary">Read More</a>
         </div>
       </div>
     </div>
 
-
-
-    <h3 className="m-2 p-3">Into the Darkness: A Chronicle of Underground for the Masses </h3>
-    <hr className="bg-white"></hr>
-      <div className='col-md-6'>
-<img src={"https://heavy-local.com/assets/Astronomica_response.jpg"} className="img-fluid"></img>
+<hr className="bg-white" />
+<h3>Tempted x Spellbound x Khirki (18/05/24)</h3>
+    <div className='col-md-6'>
+      <img src={"https://heavy-local.com/assets/437653253_1019652299623299_8618193590206532236_n.jpg"} className="img-fluid" />
       </div>
 
       <div className='col-md-6'>
         <p className="lead">
-        In the depths of Quantic Pub, on the 5th and 6th of April 2024, the inaugural edition of the festival dedicated to Black Metal unfolded its wings: “Underground for the Masses”. Over the course of two nights, thirteen diverse bands from across Europe and beyond took to the stage, bringing with them a spectrum of Black Metal styles, from Classic to Suicidal, Atmospheric to Melodic, and Black Death Metal, among others. The air was thick with anticipation as the clock struck 18:00, marking the commencement of an auditory journey into the abyss.
-        </p>
-        <a className="btn btn-danger" href="/article/Undeground-for-the-masses_archive">Read More</a>
+        Σάββατο και τι καλύτερο από ένα βραδινό live στα ανοιξιάτικα Γιάννινα! Πήγαμε στο Studio 203 δίπλα από την Παμβώτιδα για μια πραγματικά υπέροχη rock βραδιά στην οποία υπό την αιγίδα της Bullet Productions έπαιξαν τρεις πολλά υποσχόμενες μπάντες! Οι Γιαννιώτες Tempted και Spellbound έδωσαν το έναυσμα και οι Αθηναίοι Khirki την ολοκλήρωσαν με απόλυτη επιτυχία! Οι πόρτες άνοιξαν στις 8:30, το live ξεκίνησε στις 9:15 και στις 12:30 περίπου τελείωσε! Ο χώρος του Studio 203 γέμισε και το κλίμα ήταν ζεστό και οικογενειακό ενώ ο ήχος του χώρου πεντακάθαρος Πάμε να δούμε τις μπάντες μας!
+        .        </p>
+        <a className="btn btn-danger" href="/article/Tempted-x-Spellbound-x-Khirki-(18-05-24)">Read More</a>
       </div>
-    <hr className="bg-white"/>
-    <h3 className="m-2 p-3">Anormoi X EN STIGMA</h3>
-    <hr className="bg-white"></hr>
-      <div className='col-md-6'>
-      <img src={"https://heavy-local.com/assets/ANORMOI01.jpg"} className="img-fluid" />
-      </div>
-
-      <div className='col-md-6'>
-        <p className="lead">
-        Γενικώς για τη συγκεκριμένη βραδιά ένα έχω να πω: ενώ πίστευα πως είχα προετοιμαστεί σχετικά καλά για το τι θα παιζόταν, και πάλι αιφνιδιάστηκα.
-
-Τη συναυλία άνοιξαν οι En-Stigma, μία folk progressive metal μπάντα με έδρα τα Ιωάννινα, που μας εντυπωσίασαν με τα ηλεκτρικά μπουζούκια τους και τα φωνητικά της Αίγλης.        </p>
-        <a className="btn btn-danger" href="/article/Anorimoi-response-archive">Read More</a>
-      </div>
-
   </div>
 </div>
       </header>
-      <section className="live-chornicles">
-        <div className="container">
-          <h4>Live Chronicles by Daria (ENG)</h4>
-          <div className="row text-center mt-4">
-            <div className="col-md-6">
-              <img src={"https://heavy-local.com/assets/chronicles/chronicle-02/yothiria-02.jpg"} className="img-fluid" />
-            </div>
-            <div className="col-md-6">
-              <h3>
-                Chronicles of the Underworld Vol 2: Yoth Iria's Epic Odyssey at
-                Psychosounds Festival
-              </h3>
-              <a
-                href="/Chronicles_of_the_underworld_vol-2_archive"
-                className="btn btn-primary"
-              >
-                Read More
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
       <hr className="bg-dark"></hr>
       <section className="MetalLegends">
         <div className="container-fluid">
@@ -406,12 +367,8 @@ const Home = () => {
                 Σήμερα Στο Legends
               </h3>
               <p className="lead text-white">
-              Γιώργος Κόλλιας - Από τα “αλώνια” της Κορινθίας στην κορυφή της Extreme Metal
-                        </p>
-              <p className="lead text-white">
-              Σήμερα στο Legends θα γίνει μια ιδιαίτερη μνεία για έναν drummer, έναν Έλληνα drummer που έχει φτάσει να ανήκει στο πάνθεον της μέταλ και δη της death, τον Γιώργο Κόλλια! Παρόλο που πρόκειται για έναν βετεράνο καλλιτέχνη και λίαν καταξιωμένο, πασίγνωστο σε όλη την υφήλιο στους χώρους της Extreme Metal 
-              </p>
-              <a href="/legends-5-archive" className="btn btn-primary">
+              Αγαπάτε τα άτομα με ειδικές ανάγκες - Ask Tomorrow - Special Kid                        </p>
+              <a href="/article/Αγαπάτε-τα-άτομα-με-ειδικές-ανάγκες---Ask-Tomorrow---Special-Kid" className="btn btn-primary">
                 Read More
               </a>
             </div>
