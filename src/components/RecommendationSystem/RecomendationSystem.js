a31c488353e6bb035c68cc5f58ef99a2ef41e615
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-05-30 03:08:18 +0300

    Created a simple recommendation system

--- src/components/RecommendationSystem/RecomendationSystem.js
+++ src/components/RecommendationSystem/RecomendationSystem.js
@@ -1,48 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import { ref as storageRef, getDownloadURL } from "firebase/storage";
+import {ref as storageRef, getDownloadURL, listAll} from "firebase/storage";
 import {ref as dbRef, update, get, onValue} from "firebase/database";
 import {auth, database, storage} from "../../firebase";
 
 const RecommendationSystem = () => {
     const [favoriteCategories, setFavoriteCategories] = useState([]);
+    const [articles, setArticles]=useState([]);
 
-    const fetchCategories = async (userId) => {
-        const userSavedArticlesRef = dbRef(database, `users/${userId}/savedArticles`);
-        onValue(userSavedArticlesRef, async (snapshot) => {
-            const savedArticlesData = snapshot.val();
-            if (savedArticlesData) {
-                const savedArticlesList = Object.keys(savedArticlesData).map((key) => {
-                    console.log(key)
-                    return ({
-                        name: key,
-                        isEarlyAccess: savedArticlesData[key].isEarlyAccess
-                    })});
-                await fetchCategoriesFromArticles(savedArticlesList);
-            } else {
-                setFavoriteCategories([]);
-            }
-        });
-    };
+    const fetchCategories = (userId) => {
+        return new Promise((resolve, reject) => {
+            const userSavedArticlesRef = dbRef(database, `users/${userId}/savedArticles`);
+            onValue(userSavedArticlesRef, async (snapshot) => {
+                const savedArticlesData = snapshot.val();
+                if (savedArticlesData) {
+                    const savedArticlesList = Object.keys(savedArticlesData).map((key) => {
+                        console.log(key)
+                        return ({
+                            name: key,
+                            isEarlyAccess: savedArticlesData[key].isEarlyAccess
+                        })});
 
-    const fetchCategoriesFromArticles = async (savedArticlesList) => {
-        console.log(savedArticlesList)
-        const categories = [];
-        for (const article of savedArticlesList) {
-            let articleData;
-            // Read the JSON object from the corresponding folder in Firebase Storage
-            if (article.isEarlyAccess) {
-                articleData = await fetchArticleData('early_releases', article.name);
-            } else {
-                articleData = await fetchArticleData('articles', article.name);
-            }
-            console.log("Helo"+articleData)
-            // Extract categories from the article data
-            if (articleData && articleData.category) {
-                categories.push(articleData.category);
-            }
-        }
-        // Set the retrieved categories as the user's favorite categories
-        setFavoriteCategories(categories);
+
+
+                    const categories = [];
+                    for (const article of savedArticlesList) {
+                        let articleData;
+                        // Read the JSON object from the corresponding folder in Firebase Storage
+                        if (article.isEarlyAccess) {
+                            articleData = await fetchArticleData('early_releases', article.name);
+                        } else {
+                            articleData = await fetchArticleData('articles', article.name);
+                        }
+                        // Extract categories from the article data
+                        if (articleData && articleData.category) {
+                            categories.push(articleData.category);
+                        }
+                    }
+                    // Set the retrieved categories as the user's favorite categories
+                    resolve(categories);
+                } else {
+                    resolve([]);
+                }
+            });
+        })
     };
 
     const fetchArticleData = async (folder, articleName) => {
@@ -58,12 +58,60 @@ const RecommendationSystem = () => {
         }
     };
 
+
+    const readAllArticles = async () => {
+        try {
+            const articles = [];
+
+            // Read articles from 'articles' folder
+            const articlesFolderRef = storageRef(storage, 'articles');
+            const articlesList = await listAll(articlesFolderRef);
+            const articlePromises = articlesList.items.map(async (item) => {
+                const downloadUrl = await getDownloadURL(item);
+                const response = await fetch(downloadUrl);
+                const responseJSON = await response.json();
+                responseJSON.link = `/article/${item.name.replace(".json", "")}`
+                return responseJSON;
+            });
+            const articlesData = await Promise.all(articlePromises);
+            articles.push(...articlesData);
+
+            // Read articles from 'early_releases' folder
+            const earlyReleasesFolderRef = storageRef(storage, 'early_releases');
+            const earlyReleasesList = await listAll(earlyReleasesFolderRef);
+            const earlyReleasesPromises = earlyReleasesList.items.map(async (item) => {
+                const downloadUrl = await getDownloadURL(item);
+                const response = await fetch(downloadUrl);
+                const responseJSON = await response.json();
+                responseJSON.link = `/article/early/${item.name.replace(".json", "")}`
+                return responseJSON;
+            });
+            const earlyReleasesData = await Promise.all(earlyReleasesPromises);
+            articles.push(...earlyReleasesData);
+
+            return articles;
+        } catch (error) {
+            console.error('Error reading articles:', error);
+            return [];
+        }
+    };
+
     useEffect(() => {
-
-
         auth.onAuthStateChanged((user) => {
             if (user) {
-                fetchCategories(user.uid);
+                const fetchCategoriesPromise = fetchCategories(user.uid);
+                const readAllArticlesPromise = readAllArticles();
+
+                Promise.all([fetchCategoriesPromise, readAllArticlesPromise])
+                    .then(([categories, articles]) => {
+                        // Filter articles based on categories
+                        const filteredArticles = articles.filter(article => categories.includes(article.category));
+                        setArticles(filteredArticles);
+                        console.log(filteredArticles);
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                    });
             }
         });
     }, []);
@@ -72,8 +120,8 @@ const RecommendationSystem = () => {
         <div>
             <h2>Favorite Categories</h2>
             <ul>
-                {favoriteCategories.map((category, index) => (
-                    <li key={index}>{category}</li>
+                {articles.map((article, index) => (
+                    <li key={index}>{article.title}</li>
                 ))}
             </ul>
         </div>
