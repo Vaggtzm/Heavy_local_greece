d592e7527d51cbca0ffb8dac8b3c4aa84a0d4e29
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-06-16 19:44:17 +0300

    added animations

--- src/pages/Authors/Author/Author.js
+++ src/pages/Authors/Author/Author.js
@@ -6,9 +6,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './author.css';
 import {storage} from "../../../firebase";
 import {NavLink} from "react-router-dom";
+import { Card, Image, Button } from 'react-bootstrap';
+import { Tilt } from 'react-tilt'
 
 const Author = ({userId, className, rating}) => {
     const [user, setUser] = useState([]);
+
+    const defaultOptions = {
+        reverse:        false,  // reverse the tilt direction
+        max:            90,     // max tilt rotation (degrees)
+        perspective:    100,   // Transform perspective, the lower the more extreme the tilt gets.
+        scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
+        speed:          1000,   // Speed of the enter/exit transition
+        transition:     true,   // Set a transition on enter/exit.
+        axis:           null,   // What axis should be disabled. Can be X or Y.
+        reset:          true,    // If the tilt effect has to be reset on exit.
+        easing:         "cubic-bezier(.03,.98,.52,.999)",    // Easing on enter/exit.
+    }
+
 
     useEffect(() => {
         const database = getDatabase();
@@ -52,29 +67,33 @@ const Author = ({userId, className, rating}) => {
 
     return (
         <NavLink className={`${className} nav-link`} to={`/author/${userId}`}>
-            <div className={"ms-5"}>
-                <div className={"card bg-dark w-100 rounded-4 p-4"}>
-                    <div className="d-flex justify-content-center mb-4 w-100">
-                        <img
-                            src={user.photoURL}
-                            alt={user.displayName}
-                            style={{border: "10px solid grey"}}
-                            className="rounded-4 " width="150" height="150"/>
-                    </div>
-                    <h5 className="mb-3 text-white">{user.displayName} </h5>
-                    <h6 className="text-primary mb-3 text-white">{user.role}</h6>
-                    <div className={"d-flex justify-content-center"}>
-                    <hr className={"text-light w-75"}/>
-                    </div>
-                    <p className="px-xl-3 text-white w-100">
-                        <i className="fas fa-quote-left pe-2"></i>
-                        {user.bio}
-                        <i className="fas fa-quote-right pe-2"></i>
-                    </p>
-
-                    <StarRating rating={rating}></StarRating>
+            <Tilt className="Tilt" options={defaultOptions}>
+                <div className="ms-5 Tilt-inner">
+                    <Card className="shadow bg-dark text-white rounded-4 p-4 w-100">
+                        <div className="d-flex justify-content-center mb-4 w-100">
+                            <Image
+                                src={user.photoURL}
+                                alt={user.displayName}
+                                className="rounded-4"
+                                width="150"
+                                height="150"
+                                roundedCircle
+                            />
+                        </div>
+                        <Card.Body className="text-center">
+                            <Card.Title className="mb-3">{user.displayName}</Card.Title>
+                            <Card.Subtitle className="mb-3 text-primary">{user.role}</Card.Subtitle>
+                            <hr className="text-light w-75 mx-auto" />
+                            <Card.Text className="px-xl-3">
+                                <i className="fas fa-quote-left pe-2"></i>
+                                {user.bio}
+                                <i className="fas fa-quote-right ps-2"></i>
+                            </Card.Text>
+                            <StarRating rating={rating} />
+                        </Card.Body>
+                    </Card>
                 </div>
-            </div>
+            </Tilt>
         </NavLink>
     );
 };
