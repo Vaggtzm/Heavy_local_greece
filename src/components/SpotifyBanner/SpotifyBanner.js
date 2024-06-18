d726b9eb9b3e2ff060f6560118e0545e62703641
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-06-18 21:07:17 +0300

    fixed general stuff

--- src/components/SpotifyBanner/SpotifyBanner.js
+++ src/components/SpotifyBanner/SpotifyBanner.js
@@ -5,7 +5,7 @@ const SpotifyBanner = () => {
         <div>
             <iframe style={{borderRadius: "12px"}}
                     src="https://open.spotify.com/embed/playlist/6QrzcuTVOFxrgnQObOyT6q?utm_source=generator"
-                    width="100%" height="200" frameBorder="0" allowfullscreen=""
+                    width="100%" height="200" frameBorder="0" allowFullScreen=""
                     allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                     loading="lazy"
                     title={"spotify banner"}></iframe>
