2abd726ca24f330ea4073906e118d4b6b7fb8995
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-07-06 10:39:12 +0300

    fixed temporarily error

--- src/components/LanguageWrapper/NavLink.js
+++ src/components/LanguageWrapper/NavLink.js
@@ -1,15 +1,18 @@
 // CustomNavLink.js
 import React from 'react';
-import { NavLink as RouterNavLink } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import {NavLink as RouterNavLink, useLocation, useParams} from 'react-router-dom';
 
 const CustomNavLink = ({ to, children, ...props }) => {
-    const { i18n } = useTranslation();
-    const language = i18n.language;
-    const newPath = `/${language}${to}`;
+    const location = useLocation();
+
+    const params = useParams();
+
+    // Extract lang from URL params
+    const { lang } = params;
+    const url = lang ? `/${lang}${to}` : to;
 
     return (
-        <RouterNavLink to={newPath} {...props}>
+        <RouterNavLink to={url} {...props}>
             {children}
         </RouterNavLink>
     );
