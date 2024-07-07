5b090593bbbf8527b33773530ee216b9b514b26b
Vaggelis Tzimas <111343674+Vaggtzm@users.noreply.github.com>
2024-07-07 18:00:26 +0300

    Update ReviewsBanner.js

--- src/components/HomeCompoments/TopNewsFeed/NewsCompoments/ReviewsBanner.js
+++ src/components/HomeCompoments/TopNewsFeed/NewsCompoments/ReviewsBanner.js
@@ -13,14 +13,14 @@ const ReviewsBanner = () => {
         <div className="col-md-6">
         <div className="CollageBox d-flex flex-wrap">
             <div className="Img-box m-1">
-                <img src="https://firebasestorage.googleapis.com/v0/b/heavy-local-12bc4.appspot.com/o/images%2F286849395_673615767140147_5560296650843229742_n_800x600.jpg?alt=media&token=ecf484b5-fc3d-4877-b3a8-a122ca53f683" />
+                <img src="https://storage.googleapis.com/heavy-local-12bc4.appspot.com/images/a2851302924_16_800x800.jpg?GoogleAccessId=firebase-adminsdk-asgq1%40heavy-local-12bc4.iam.gserviceaccount.com&Expires=1740787200&Signature=W0t%2FM0I7L58NecCnfvWM2bOOWuN82IdOHO87WXdnp8vJ%2FvuL10j%2BLdYn%2BEyM2SMvXkyL%2B30W%2BYV4MNCrOnWNc9b6XlMCajOJs3T5CBZoXGpEWClJuWkJ0qYUv5ie9d06Q3P5m3e1bRfUZ70XqVQL4gWvVBFLZNi7q3mjsgKyD39DttfTnOiw6ldrGt1LzLwJ9tIW9C457PLOVng1t0lDfkco0fuUlcomSNMHHfpGuQwCPCdoiLotEanexMM1COwyYGgB2lcNE%2F87u4Moj9Or0orPvxLieJzHGZNtebXqsA4Zyw%2B43wmo%2B01bfdwJxIDge95tjoYo9Ooi6r%2Bof616ow%3D%3D" />
             </div>
             <div className="Img-box">
-            <img src="https://firebasestorage.googleapis.com/v0/b/heavy-local-12bc4.appspot.com/o/images%2F286849395_673615767140147_5560296650843229742_n_800x600.jpg?alt=media&token=ecf484b5-fc3d-4877-b3a8-a122ca53f683" />
+            <img src="https://storage.googleapis.com/heavy-local-12bc4.appspot.com/images/Of_the_Muses_800x800.jpeg?GoogleAccessId=firebase-adminsdk-asgq1%40heavy-local-12bc4.iam.gserviceaccount.com&Expires=1740787200&Signature=QCT9pAPG5UgdkZpHfoGOwDT6Ij2BgDY30UxuEcRhwxeSDsWHJyQKTZr4Tvz6SBkOCmATSRgAXKW7UztUj5Bt7oavA24vc8pQCl%2F09UUnWt%2B3G%2B1PYsJYr9RxRfbzQqO6vZALLBlckcdN4pJEFvHevCFqJSYoAYqp6Jozj1vVEi4z%2FmTzLyj1bhxhP0z2949x7MMQpdSSW0c1TpVmEQeFzS%2BPzLLqviBBd69X2Dh51B4oxMGsjGMTmHI9Y6cBbsVfYH1vlr1IrWPId8ITjmubCsyBJxXG4XM3Pnu7%2F9MTHoXPR6o55GLMLKi4YCH17wLu5IlESQrPirtL2vgKKUBvdw%3D%3D" />
 
             </div>
             <div className="Img-box">
-            <img src="https://firebasestorage.googleapis.com/v0/b/heavy-local-12bc4.appspot.com/o/images%2F286849395_673615767140147_5560296650843229742_n_800x600.jpg?alt=media&token=ecf484b5-fc3d-4877-b3a8-a122ca53f683" />
+            <img src="https://storage.googleapis.com/heavy-local-12bc4.appspot.com/images/Acyl_800x800.webp?GoogleAccessId=firebase-adminsdk-asgq1%40heavy-local-12bc4.iam.gserviceaccount.com&Expires=1740787200&Signature=VeGLmUhQrJUc4jb8mYgF%2Bm0M10M8BKCe9eu63CusW5YsObWCSc3aqeeHBGdnPhu%2FirGszbrpA0GS6RtXyxjqTd%2BN3iz0o%2BDcP%2FajegJKn53h6Rv5%2BpDYI5ZThhMbUyXm%2F9zAB3Ssh9n%2FW9%2Bn%2BzBRPRnxCA7onmkT7oXjZsTOud9n%2Fa7CeCPeOTEijI5afUumvwt9ijKGIv8Es3DDQoYE4R6HKYZnddNS0zdKhOMmYv%2Fl0wGphFK%2FQtwvmBmZNVUoU%2FueoX%2FGzJlJAkOAzNExDnsk30WG%2FjJZd4mtspyJmoycRuzIUjk8ign5quqMxay15YdjyJj1QZDcHzBf5rbz0A%3D%3D" />
             </div>
 
         </div>
@@ -34,7 +34,7 @@ const ReviewsBanner = () => {
 
      </p>
      <div className="Button Box d-flex justify-content-center align-items-center">
-     <NavLink  className="btn btn-danger">
+     <NavLink to="https://pulse-of-the-underground.com/articles-page" className="btn btn-danger">
       Explore 
      </NavLink>
      </div>
