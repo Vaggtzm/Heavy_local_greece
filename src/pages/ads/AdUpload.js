510d86b73e6b15a3af3938f208674d8cc5740ce2
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-07-07 15:10:38 +0300

    added components for ads

---
+++ src/pages/ads/AdUpload.js
@@ -0,0 +1,124 @@
+import React, {useEffect, useState} from 'react';
+import {get, getDatabase, push, ref} from 'firebase/database';
+import {getDownloadURL, getStorage, ref as storageRef, uploadBytes} from 'firebase/storage';
+import {Alert, Button, Container, Form} from 'react-bootstrap';
+import {auth} from "../../firebase";
+import ImageUpload from "../../components/UploadSystem/components/fancyImage/ImageUpload";
+import "../../components/UploadSystem/components/fancyImage/ImageUpload.css";
+import {useTranslation} from "react-i18next";
+
+const AdUpload = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState(null);
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [message, setMessage] = useState('');
+    const db = getDatabase();
+    const storage = getStorage();
+    const [currentUser, setCurrentUser] = useState(null);
+
+    const [t] = useTranslation();
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(async (user) => {
+            if (!user) {
+                window.location.href = '/User/login';
+                return;
+            }
+            setCurrentUser(user);
+
+            // Check if user is admin
+            const adminRef = ref(db, 'roles/admin');
+            const snapshot = await get(adminRef);
+            if (snapshot.exists()) {
+                const admins = snapshot.val();
+                if (admins.includes(user.email)) {
+                    setIsAdmin(true);
+                }
+            }
+        });
+
+        return () => unsubscribe();
+    }, [db]);
+
+    const handleImageChange = (e) => {
+        if (e.target.files[0]) {
+            setImage(e.target.files[0]);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (image) {
+            const imageRef = storageRef(storage, `ads/${image.name}`);
+            await uploadBytes(imageRef, image);
+            const imageURL = await getDownloadURL(imageRef);
+
+            const adData = {
+                title,
+                description,
+                imageURL,
+                userId: currentUser.uid,
+                status: 'pending' // Set status to pending approval
+            };
+
+            const adsRef = ref(db, 'ads');
+            await push(adsRef, adData);
+
+            setTitle('');
+            setDescription('');
+            setImage(null);
+            setMessage('Your ad has been submitted and is pending approval.');
+        }
+    };
+
+    return (
+        <Container className={"mb-5"}>
+
+            {message && <div className={"d-flex justify-content-center"}><Alert className={"mt-4 mb-4 w-50 text-center"}
+                                                                                variant="info">{message}</Alert></div>}
+            <div className={"d-flex justify-content-center"}>
+                <Form className={"card bg-dark p-4 w-75"} onSubmit={handleSubmit}>
+                    <Form.Group controlId="formAdTitle">
+                        <Form.Label className={"text-white"}>{t("Title")}</Form.Label>
+                        <Form.Control
+                            type="text"
+                            className={"form-control"}
+                            placeholder="Enter title"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            required
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formAdDescription">
+                        <Form.Label className={"text-white"}>{t("description")}</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            className={"form-control"}
+                            rows={3}
+                            placeholder="Enter description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            required
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formAdImage">
+                        <ImageUpload setImage={setImage} image={image}/>
+                    </Form.Group>
+                    <div className={"d-flex justify-content-center"}>
+                        <div className={"w-25"}>
+                            <Button className={"btn-lg"} variant="primary" type="submit">
+                                {t("submit")}
+                            </Button>
+                        </div>
+                    </div>
+                </Form>
+            </div>
+        </Container>
+    );
+};
+
+export default AdUpload;
---
+++ src/pages/ads/AdView.js
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2024. MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+import React, { useEffect, useState } from 'react';
+import { ref, onValue, update, get, remove } from 'firebase/database';
+import { getStorage, ref as storageRef, deleteObject } from 'firebase/storage';
+import { Container, Card, Row, Col, Button } from 'react-bootstrap';
+import { auth, database } from "../../firebase";
+
+const AdView = () => {
+    const [ads, setAds] = useState([]);
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribeAuth = auth.onAuthStateChanged(async (user) => {
+            if (!user) {
+                window.location.href = '/User/login';
+                return;
+            }
+            setCurrentUser(user);
+
+            // Check if user is admin
+            const adminRef = ref(database, 'roles/admin');
+            const snapshot = await get(adminRef);
+            if (snapshot.exists()) {
+                const admins = snapshot.val();
+                if (admins.includes(user.email)) {
+                    setIsAdmin(true);
+                }
+            }
+        });
+
+        const adsRef = ref(database, 'ads');
+
+        const unsubscribeAds = onValue(adsRef, (snapshot) => {
+            const adsData = snapshot.val();
+            const adsList = [];
+
+            for (let key in adsData) {
+                const ad = adsData[key];
+                const adWithKey = { ...ad, key };
+                adsList.push(adWithKey);
+            }
+
+            setAds(adsList);
+        });
+
+        return () => {
+            unsubscribeAuth();
+            unsubscribeAds();
+        };
+    }, []);
+
+    const handleApproval = async (id, approved) => {
+        const adRef = ref(database, `ads/${id}`);
+        if (approved) {
+            await update(adRef, { status: 'approved' });
+            setAds(ads.map(ad => ad.key === id ? { ...ad, status: 'approved' } : ad));
+        } else {
+            // Delete the ad and its associated image if rejected
+            const ad = ads.find(ad => ad.key === id);
+            const storage = getStorage();
+            const imageRef = storageRef(storage, ad.imageURL);
+            await deleteObject(imageRef);
+            await remove(adRef);
+            setAds(ads.filter(ad => ad.key !== id));
+        }
+    };
+
+    const handleDelete = async (id) => {
+        const ad = ads.find(ad => ad.key === id);
+        const adRef = ref(database, `ads/${id}`);
+        const storage = getStorage();
+        const imageRef = storageRef(storage, ad.imageURL);
+        await deleteObject(imageRef);
+        await remove(adRef);
+        setAds(ads.filter(ad => ad.key !== id));
+    };
+
+    const sortedAds = ads.sort((a, b) => {
+        if (isAdmin) {
+            return a.status === 'pending' ? -1 : 1;
+        } else {
+            return a.userId === currentUser.uid ? -1 : 1;
+        }
+    });
+
+    return (
+        <Container>
+            <Row>
+                {sortedAds.map((ad) => (
+                    (ad.status === 'approved' || isAdmin) && (
+                        <Col key={ad.key} sm={12} md={6} lg={4}>
+                            <Card className="mb-4">
+                                <Card.Img variant="top" src={ad.imageURL} />
+                                <Card.Body>
+                                    <Card.Title>{ad.title}</Card.Title>
+                                    <Card.Text>{ad.description}</Card.Text>
+                                    <div className={"w-100 d-flex justify-content-evenly"}>
+                                        {isAdmin && ad.status === 'pending' && (
+                                            <Button variant="success"
+                                                    onClick={() => handleApproval(ad.key, true)}>Approve</Button>
+                                        )}
+                                        {(currentUser && (currentUser.uid === ad.userId || isAdmin)) && (
+                                            <Button variant="danger"
+                                                    onClick={() => handleDelete(ad.key)}>Delete</Button>
+                                        )}
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
+                ))}
+            </Row>
+        </Container>
+    );
+};
+
+export default AdView;
