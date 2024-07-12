e7b9bf71bdcbb5818edd363b22aea6d3fd3f6996
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-07-12 18:11:56 +0300

    Organized menus to dropdowns to be more organized

--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -5,7 +5,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import InstallButton from "../PWAinstal/pwaInstall";
 import {getIdTokenResult, signOut} from "firebase/auth";
-import {Button} from "react-bootstrap";
+import {Button, NavDropdown} from "react-bootstrap";
 import {onValue, ref} from "firebase/database";
 import './Observer.css';
 import NavLink from "../LanguageWrapper/NavLink";
@@ -19,11 +19,14 @@ const AppNavigation = ({menuVisible}) => {
     const [isLeader, setIsLeader] = useState(false);
     const [isCommentAdmin, setIsCommentAdmin] = useState(false);
 
+    const [displayName, setDisplayName] = useState("");
+
     const navigate = useNavigate()
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(async (user) => {
             if (user) {
+                setDisplayName(user.displayName?user.displayName:"Unknown");
                 const userList = ref(database, 'roles');
                 onValue(userList, async (snapshot) => {
                     const roles = snapshot.val();
@@ -62,60 +65,78 @@ const AppNavigation = ({menuVisible}) => {
     }, []);
 
 
-
-
     return (
         <>
-            <Navbar expand="lg" className={`sticky-top overflow-hidden ${menuVisible ? 'visible' : 'hidden'}`} variant="dark" style={{ backgroundColor: "rgba(0,0,0,0.85)", zIndex:99999 }}>
-                <Container fluid>
+            <Navbar expand="lg" className={`sticky-top ${menuVisible ? 'visible' : 'hidden'}`} variant="dark"
+                    style={{backgroundColor: "rgba(0,0,0,0.85)", zIndex: 99999}}>
+                <Container fluid >
                     <NavLink to={(loggedIn) ? "/User/home" : "/"} className="navbar-brand">
                         <img
                             src={"https://pulse-of-the-underground.com/assets/PulseOfTheUnderground.jpg"}
                             className="img-fluid rounded-circle"
-                            style={{ maxWidth: "100px", maxHeight: "100px" }}
+                            style={{maxWidth: "100px", maxHeight: "100px"}}
                             alt="Navbar Brand"
                         />
                     </NavLink>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="d-flex justify-content-center justify-content-evenly w-100 table-hover">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse style={{flexFlow: "column"}} id="basic-navbar-nav">
+                        <Nav className="d-flex align-items-center justify-content-evenly w-100 table-hover">
                             <NavLink to={(loggedIn) ? "/User/home" : "/"}
                                      className='nav-link text-white '>Home</NavLink>
-                            {(loggedIn) &&
-                                <NavLink to="/User/Saved" className='nav-link text-white'>Saved Articles</NavLink>}
-                            <NavLink to={"/articles-page"} className='nav-link text-white'>Articles</NavLink>
+
+                            <NavDropdown className={"text-white"} title="Articles" >
+                                {(loggedIn) &&
+                                    <NavLink to="/User/Saved" className='nav-link bg-dark text-white'>Saved
+                                        Articles</NavLink>}
+                                <NavLink to={"/articles-page"}
+                                         className='nav-link bg-dark text-white'>All Articles</NavLink>
+                                {isAuthor && (
+                                    <NavLink to="/upload" className='nav-link bg-dark text-white'>Upload</NavLink>
+                                )}
+                                {isTranslator &&
+                                    <NavLink to={"/upload/translation"} className='nav-link bg-dark text-white'>Translation
+                                        System</NavLink>
+                                }
+                            </NavDropdown>
+
+
                             <NavLink to={"/Art-Gallery-page"} className='nav-link text-white'>Art Gallery</NavLink>
                             <NavLink to={"/gigs"} className='nav-link text-white'>Gigs</NavLink>
                             <NavLink to={"/ads"} className='nav-link text-white'>Ads By You</NavLink>
                             <NavLink to="/profile" className='nav-link text-white'>Profile</NavLink>
                             <InstallButton/>
-                            {isAuthor && (
-                                    <NavLink to="/upload" className='nav-link text-white'>Upload</NavLink>
-                            )}
+                            {(isAdmin|| isLeader|| isCommentAdmin) &&
+                            <NavDropdown className={"text-white"} title="Administration" >
+                                {isCommentAdmin && (
+                                    <NavLink to="/admin/comments" className='nav-link text-white bg-dark'>Reported
+                                        Comments</NavLink>
+                                )}
 
-                            {isCommentAdmin && (
-                                <NavLink to="/admin/comments" className='nav-link text-white'>Reported Comments</NavLink>
-                            )}
+                                {(isAdmin) &&
+                                    <NavLink to="/admin" className='nav-link text-white bg-dark'>User Administration</NavLink>
+                                }
 
-                            {(isAdmin) &&
-                                <NavLink to="/admin" className='nav-link text-white'>User Administration</NavLink>
-                            }
+                                {(isAdmin || isLeader) &&
+                                    <NavLink to="/upload/admin" className='nav-link text-white bg-dark'>Admin Dashboard</NavLink>
+                                }
+                            </NavDropdown>}
 
-                            {(isAdmin || isLeader) &&
-                                <NavLink to="/upload/admin" className='nav-link text-white'>Admin Dashboard</NavLink>
-                            }
-                            {isTranslator &&
-                                <NavLink to={"/upload/translation"} className='nav-link text-white'>Translation
-                                    System</NavLink>
-                            }
 
                             {(loggedIn) ? (
-                                <Nav.Link onClick={() => {
-                                    signOut(auth).then(() => {
-                                        navigate("/")
-                                    })
-                                }} className='nav-link text-white'><Button variant="outline-danger">Sign
-                                    Out</Button></Nav.Link>
+
+                                <div className='nav-link text-white d-flex flex-column align-items-center w-25'>
+                                    <Navbar.Text className={"small w-100 m-0"}>
+                                        Logged in as: <br/><span><NavLink to="/profile" className="nav-link small w-100">{displayName}</NavLink></span>
+                                    </Navbar.Text>
+                                    <div className=" w-100">
+                                        <Button size={"sm"} onClick={() => {
+                                            signOut(auth).then(() => {
+                                                navigate("/")
+                                            })
+                                        }} variant="outline-danger">Sign Out</Button>
+                                    </div>
+                                </div>
+
                             ) : (<>
                                     <NavLink to="/User/register" className='nav-link text-white link'>Create
                                         Account</NavLink>
