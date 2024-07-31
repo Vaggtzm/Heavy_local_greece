586dd506b05c8201075a9b7a186f340b524e187d
Vaggelis Tzimas <111343674+Vaggtzm@users.noreply.github.com>
2024-07-31 21:41:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -184,27 +184,27 @@ function App() {
     }
 
 
-    return (
+   /// return (
 
-        <div className="d-flex flex-column h-100">
-            <div ref={placeholderRef} style={{height: '1px'}}></div>
-            <div className="flex-grow-1">
-                <NotificationToast/>
-                <AppNavigation menuVisible={menuVisible}/>
+       // <div className="d-flex flex-column h-100">
+         //   <div ref={placeholderRef} style={{height: '1px'}}></div>
+           /// <div className="flex-grow-1">
+              ///  <NotificationToast/>
+                //<AppNavigation menuVisible={menuVisible}/>
 
-                <Routes>
-                    {getRoutes(true)}
-                    {getRoutes(false)}
-                    <Route path="*" element={<Navigate to="/404"/>}/>
-                </Routes>
-            </div>
-            {/**
-             Ραδιόφωνο. Το πας όπου θες
-             **/}
+                //<Routes>
+                  //  {getRoutes(true)}
+                   // {getRoutes(false)}
+                    //<Route path="*" element={<Navigate to="/404"/>}/>
+                //</Routes>
+            //</div>
+            //{/**
+             //Ραδιόφωνο. Το πας όπου θες
+             *//*/}
 
-            {/** <Footer footerVisible={menuVisible}/> */}
-        </div>
-    );
+            //{/** <Footer footerVisible={menuVisible}/> */}
+       // </div>
+    //);
 }
 
 
