c16ebc2fdb6e55a48676bdfe6467b8aba5060fd4
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-08-15 13:13:54 +0300

    Automate articles

--- src/components/HomeCompoments/TopNewsFeed/TopNews.js
+++ src/components/HomeCompoments/TopNewsFeed/TopNews.js
@@ -1,228 +1,117 @@
-import React, {useEffect, useRef, useState} from "react";
-import {useTranslation} from "react-i18next";
-import "./Animation.css";
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import NavLink from "../../LanguageWrapper/NavLink";
-import ReviewsBanner from "./NewsCompoments/ReviewsBanner";
-import POUtransparent from "./../../../assets/PulseOfTheUnderground-removebg-preview.png"
 import Sponshorbanner from "./NewsCompoments/SponshorBanner";
-import Contact from "../../ContactForm/contact";
-
+import "./TopNews.css";
+import { database } from "../../../firebase";
+import { get, ref } from "firebase/database";
 
 const TopNews = () => {
-    const {t} = useTranslation();
-    const [visible, setVisible] = useState(false);
-    const topNewsRef = useRef(null);
+    const { t } = useTranslation();
+    const [articles, setArticles] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('All');
 
     useEffect(() => {
-        const handleScroll = () => {
-            if (topNewsRef.current) {
-                const topNewsHeight = topNewsRef.current.getBoundingClientRect().top;
-                const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-                setVisible(scrollTop > topNewsHeight);
+        const fetchArticles = async () => {
+            try {
+                const categoriesSnapshot = await get(ref(database, '/articlesListLatest/articles'));
+                const categoriesData = categoriesSnapshot.val();
+                const categoriesList = ['All', ...Object.keys(categoriesData)];
+                setCategories(categoriesList);
+
+                const articlesPromises = categoriesList.filter(category => category !== 'All').map(async (category) => {
+                    const articlesSnapshot = await get(ref(database, `/articlesList/articles/${category}`));
+                    const articlesData = articlesSnapshot.val();
+
+                    const filteredArticles = Object.keys(articlesData)
+                        .filter(articleName => categoriesData[category].includes(articleName))
+                        .reduce((acc, articleName) => {
+                            acc[articleName] = articlesData[articleName];
+                            return acc;
+                        }, {});
+
+                    return { category, articles: filteredArticles };
+                });
+
+                const articlesResults = await Promise.all(articlesPromises);
+                const allArticles = articlesResults.flatMap(result => Object.keys(result.articles).map(articleKey => {
+                    const article = result.articles[articleKey];
+                    return ({
+                        ...article,
+                        category: result.category,
+                        link: articleKey
+                    });
+                }));
+                setArticles(allArticles);
+            } catch (error) {
+                console.error("Error fetching articles:", error);
             }
         };
 
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
+        fetchArticles();
     }, []);
 
+    const handleCategoryChange = (category) => {
+        setSelectedCategory(category);
+    };
+
+    const filteredArticles = selectedCategory === 'All'
+        ? articles
+        : articles.filter(article => article.category === selectedCategory);
+
     return (
-        <div ref={topNewsRef} className={`top-news-container ${visible ? "visible" : ""}`}>
-            <ReviewsBanner/>
-            <hr className="bg-white"/>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <h2 className="text-white">Latest News</h2>
-                    </div>
-
-
-
-
-
-                    <div className="row">
-                        {/** Main Content Column */}
-                        <div className="col-md-8">
-                            {/**Interviews Start (ENG FIRST ) */}
-                       
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <img
-                                        src="https://firebasestorage.googleapis.com/v0/b/heavy-local-12bc4.appspot.com/o/images%2FNEW-ALBUM%3A-Finnish-Black-Metal-Band-Luuskki-to-Release-Self-Titled-Debut-Album-in-October--First-Single-%22Fifth-of-the-Fifth-Columns%22-Out-Now!_800x800?alt=media&token=54bbcc05-b649-4521-94ba-ed543badd511"
-                                        className="img-fluid w-50 m-2"/>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="CustomBorder">
-                                        <h3><span className="text-danger bold">REVIEW(ENG):</span></h3>
-                                        <p className="lead text-white">
-                                        SINGLE REVIEW: Denigrate's "Crystal Rest" — A Dark Fusion of Crushing Metal and Stunning Visuals                                        
-                                        </p>
-                                        <NavLink className="btn btn-danger"
-                                                 to={"https://pulse-of-the-underground.com/article/NEW-ALBUM:-Finnish-Black-Metal-Band-Luuskki-to-Release-Self-Titled-Debut-Album-in-October--First-Single-%22Fifth-of-the-Fifth-Columns%22-Out-Now!"}>
-                                            Read More
-                                        </NavLink>
-                                    </div>
-                                </div>
-                            </div>
-
-                            
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <img
-                                        src="https://pulse-of-the-underground.com/assets/Kreyl:-Medusa's-Death-(Single-Review)"
-                                        className="img-fluid w-50 m-2"/>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="CustomBorder">
-                                        <h3><span className="text-danger bold">REVIEW(ENG):</span></h3>
-                                        <p className="lead text-white">
-                                        SIGNLE REVIEW :Kreyl- Medusa's Death                                        </p>
-                                        <NavLink className="btn btn-danger"
-                                                 to={"/article/Kreyl:-Medusa's-Death-(Single-Review)"}>
-                                            Read More
-                                        </NavLink>
-                                    </div>
-                                </div>
-                            </div>
-
-                            
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <img
-                                        src="https://pulse-of-the-underground.com/assets/REVIEW:--ORKRIST----single-Prince-of-Melancholy----LUEA"
-                                        className="img-fluid w-50 m-2"/>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="CustomBorder">
-                                        <h3><span className="text-danger bold">REVIEW(GR):</span></h3>
-                                        <p className="lead text-white">
-                                        SINGLE REVIEW: Οι ORKRIST παρουσιάζουν το  single “Prince of Melancholy” από το άλμπουμ “LUEA”.                                       </p>
-                                        <NavLink className="btn btn-danger"
-                                                 to={"/article/REVIEW:--ORKRIST----single-Prince-of-Melancholy----LUEA"}>
-                                            Read More
-                                        </NavLink>
-                                    </div>
-                                </div>
-                            </div>
-
-                    
-                            {/**Interviews END */}
-
-
-                            {/**Reviews Start  (ENG FIRST) */}
-
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <img
-                                        src="https://pulse-of-the-underground.com/assets/Band-Review:-Selofan   "
-                                        className="img-fluid w-50 m-2"/>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="CustomBorder">
-                                        <h3><span className="text-danger bold">REVIEW(GR):</span></h3>
-                                        <p className="lead text-white">
-                                        Band Review: Selofan
-                                        </p>
-                                        <NavLink className="btn btn-danger"
-                                                 to={"/article/Band-Review:-Selofan"}>
-                                            Read More
-                                        </NavLink>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <img
-                                        src="https://pulse-of-the-underground.com/assets/none.jpg"
-                                        className="img-fluid w-50 m-2"/>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="CustomBorder">
-                                        <h3><span className="text-danger bold">REVIEW(GR):</span></h3>
-                                        <p className="lead text-white">
-                                        END OF AN ERA: None Bids Farewell After Four Albums                                         </p>
-                                        <NavLink className="btn btn-danger"
-                                                 to={"/article/END-OF-AN-ERA:-None-Bids-Farewell-After-Four-Albums"}>
-                                            Read More
-                                        </NavLink>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <img
-                                        src="https://pulse-of-the-underground.com/assets/NEWS:Vision-Bleak--Weird-Tales"
-                                        className="img-fluid w-50 m-2"/>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="CustomBorder">
-                                        <h3><span className="text-danger bold">NEWS(ENG):</span></h3>
-                                        <p className="lead text-white">
-                                        NEWS:Vision Bleak -Weird Tales                                       
-                                         </p>
-                                        <NavLink className="btn btn-danger"
-                                                 to={"/article/NEWS:Vision-Bleak--Weird-Tales"}>
-                                            Read More
-                                        </NavLink>
-                                    </div>
-                                </div>
-                            </div>
-
-
-
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <img
-                                        src="https://firebasestorage.googleapis.com/v0/b/heavy-local-12bc4.appspot.com/o/images%2FGlassCare.jpg?alt=media&token=f5e4d929-68b3-44f8-bf99-da8d2f9498b2"
-                                        className="img-fluid w-50 m-2"/>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="CustomBorder">
-                                        <h3><span className="text-danger bold">NEWS(GR):</span></h3>
-                                        <p className="lead text-white">
-                                        NEWS: DECAIR – single “Glasscase”
-                                        </p>
-                                        <NavLink className="btn btn-danger"
-                                                 to={"/article/GLASSCASE"}>
-                                            Read More
-                                        </NavLink>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <img
-                                        src="https://pulse-of-the-underground.com/assets/unnamed.jpg"
-                                        className="img-fluid w-50 m-2"/>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="CustomBorder">
-                                        <h3><span className="text-danger bold">NEWS(ENG):</span></h3>
-                                        <p className="lead text-white">
-                                        SHINIGAMI | HELLPASS | PRIMAL ROOTS | JEHOVAH ON DEATH – live @ Temple Athens – Saturday September 21                                         </p>
-                                        <NavLink className="btn btn-danger"
-                                                 to={"/article/-SHINIGAMI-|-HELLPASS-|-PRIMAL-ROOTS-|-JEHOVAH-ON-DEATH--live-@-Temple-Athens--Saturday-September-21-"}>
-                                            Read More
-                                        </NavLink>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                      
-                    </div>
-
-
-                    {/**News Start */}
-                
-                    {/* Empty For Now */}
-                    {/**News End */}
-                    <Sponshorbanner />
-
-                </div>
+        <div className="top-news-wrapper mt-5 container rounded-3">
+            <div className="top-news-header d-flex justify-content-between align-items-center mb-4">
+                <h1 className="top-news-title">{t('Latest Articles')}</h1>
+                <NavLink to="/articles-page" className="btn explore-btn">
+                    Explore More
+                </NavLink>
             </div>
+
+            <div className="top-news-category-menu d-flex flex-wrap mb-4">
+                {categories.map((category) => (
+                    <button
+                        key={category}
+                        className={`category-button btn ${selectedCategory === category ? 'btn-primary text-white' : 'btn-outline-light'}`}
+                        onClick={() => handleCategoryChange(category)}
+                    >
+                        {t(category)}
+                    </button>
+                ))}
+            </div>
+
+            <div className="top-news-articles">
+                {filteredArticles.length > 0 ? (
+                    <div className="row justify-content-center">
+                        {filteredArticles.map((article, index) => (
+                            <div
+                                key={index}
+                                className={`top-news-article-card p-2 pb-3 col-md-4 col-sm-6`}
+                            >
+                                <div className="top-news-card bg-dark text-white shadow-lg">
+                                    <img
+                                        src={article.image}
+                                        className={"top-news-card-img img-fluid card-img"}
+                                        alt={article.title}
+                                    />
+                                    <div className="top-news-card-body">
+                                        <h5 className="top-news-card-title">{article.title}</h5>
+                                    </div>
+                                    <div className="top-news-card-footer d-flex justify-content-center pb-3">
+                                        <NavLink className="btn read-more-btn" to={`/article/${article.link}`}>
+                                            Read More
+                                        </NavLink>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <p className="no-articles-text text-center">No articles available</p>
+                )}
+            </div>
+            <Sponshorbanner />
         </div>
     );
 };
