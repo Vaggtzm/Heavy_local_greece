442f83af80ee4e4cdfe9aa0ff914ab676d9840b0
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-08-18 20:10:55 +0300

    fixed overflow in carousel

--- src/components/HomeCompoments/ExploreMore/ExpoloreMore.js
+++ src/components/HomeCompoments/ExploreMore/ExpoloreMore.js
@@ -38,7 +38,10 @@ const ExploreMore = () => {
           <div className="gallery-section">
             <h3 className="sub-title">{t("galleryTitle")}</h3>
             <hr className="divider" />
+            <div className={"m-0 mx-md-3 p-3 p-md-2"}>
+
             <SecondaryCarousel />
+            </div>
           </div>
 
           <div className="team-section m-3">
