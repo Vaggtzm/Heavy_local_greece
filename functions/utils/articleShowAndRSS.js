758cab7bcab131597530cdfdff1093c7f3fcb72d
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-08-20 19:01:12 +0300

    fixed path reference

--- functions/utils/articleShowAndRSS.js
+++ functions/utils/articleShowAndRSS.js
@@ -28,6 +28,7 @@ const express = require("express");
 const app = express();
 const RSS = require("rss");
 const fs = require("fs");
+const {resolve} = require("path");
 
 
 app.get("/feed", async (req, res) => {
@@ -110,7 +111,7 @@ app.get("/feed", async (req, res) => {
 });
 
 const getArticle = async (req, res, folder) => {
-    const filepath = path.resolve(__dirname, "index.html");
+    const filepath = resolve(__dirname, "index.html");
     const name = req.params.article;
     console.log("Hello, file requested is: " + JSON.stringify(req.params));
 
