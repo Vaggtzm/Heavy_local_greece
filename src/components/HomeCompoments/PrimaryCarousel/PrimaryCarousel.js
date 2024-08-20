c01f8c3ff309b747644738fd280a10b0b2e44852
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-08-20 13:50:29 +0300

    Updated files to load carousel from gigs

--- src/components/HomeCompoments/PrimaryCarousel/PrimaryCarousel.js
+++ src/components/HomeCompoments/PrimaryCarousel/PrimaryCarousel.js
@@ -3,44 +3,70 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../carousel/carousel.css';
-import { getDownloadURL, ref } from 'firebase/storage';
-import { storage } from '../../../firebase';
-import {Button, Modal} from "react-bootstrap";
-import {getFirebaseStorageUrlFull} from "../../../systems/UploadSystem/articleData/articleData";
+import { Button, Modal } from "react-bootstrap";
+import { getFirebaseStorageUrlFull } from "../../../systems/UploadSystem/articleData/articleData";
+import {database} from "../../../firebase";
+import {get, orderByChild, query, ref} from "firebase/database";
 
 const PrimaryCarousel = ({ customSettings, customImages, classNameImages, shouldBeFull }) => {
     const [images, setImages] = useState([]);
     const [fullImages, setFullImages] = useState([]);
     const [selectedImage, setSelectedImage] = useState(null);
     const [showModal, setShowModal] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchImages = async (imagePaths) => {
-            const imageUrls = await Promise.all(imagePaths.map(imagePath => getFirebaseStorageUrlFull(imagePath, !!shouldBeFull)));
-            setImages(imageUrls);
-            if(shouldBeFull){
-                setFullImages(imageUrls);
-            }else{
-                const fullImageUrls = await Promise.all(imagePaths.map(imagePath => getFirebaseStorageUrlFull(imagePath, true)));
-                setFullImages(fullImageUrls);
+            console.time("Fetching Images");
+
+            try {
+                // Measure time for URL fetching
+                console.time("Fetching Image URLs");
+                const imageUrls = await Promise.all(imagePaths.map(imagePath => getFirebaseStorageUrlFull(imagePath, !!shouldBeFull)));
+                console.timeEnd("Fetching Image URLs");
+
+                setImages(imageUrls);
+                setLoading(false);
+
+                if (!shouldBeFull) {
+                    console.time("Fetching Full Image URLs");
+                    const fullImageUrls = await Promise.all(imagePaths.map(imagePath => getFirebaseStorageUrlFull(imagePath, true)));
+                    console.timeEnd("Fetching Full Image URLs");
+
+                    setFullImages(fullImageUrls);
+                }
+            } catch (error) {
+                console.error('Error fetching images:', error);
+            }
+
+            console.timeEnd("Fetching Images");
+        };
+
+        const fetchData = async () => {
+            const thumbnailsRef = query(ref(database, '/gigs'), orderByChild('thumbnail'));
+
+            try {
+                console.time("Carousel Loading");
+                console.time("Firebase Get Request");
+                const snapshot = await get(thumbnailsRef);
+                console.timeEnd("Firebase Get Request");
+                const data = snapshot.val();
+                const thumbnailList = Object.keys(data).map(itemKey => `/gigs/${itemKey}/${data[itemKey].thumbnail}`);
+                fetchImages(thumbnailList).then(()=>{
+                    console.timeEnd("Carousel Loading");
+                });
+
+            } catch (error) {
+                console.error('Error fetching data:', error);
             }
         };
 
         if (customImages && customImages.length > 0) {
             fetchImages(customImages);
         } else {
-            fetchImages([
-                'images/Concerts/workInProgress/DSC_0016.JPG',
-                'images/Concerts/workInProgress/DSC_0058.JPG',
-                'images/Concerts/workInProgress/DSC_0028.JPG',
-            ]);
+            fetchData();
         }
     }, [customImages]);
-
-    const getImageSource = async (imagePath) => {
-        const imageRef = ref(storage, imagePath);
-        return await getDownloadURL(imageRef);
-    };
 
     const defaultSettings = {
         dots: false,
@@ -86,23 +112,32 @@ const PrimaryCarousel = ({ customSettings, customImages, classNameImages, should
 
     return (
         <>
-        <Slider {...settings}>
-            {images.map((image, index) => (
-                <div key={index} className={`bg-dark carousel-item ${classNameImages}`} onClick={() => handleImageClick(index)}>
-                    <img src={image} alt={`slide${index}`} />
+            {loading ? (
+                <div className="d-flex justify-content-center align-items-center" style={{ height: '300px' }}>
+                    <div className="spinner-grow text-secondary" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
                 </div>
-            ))}
-        </Slider>
+            ) : (
+                <>
+                    <Slider {...settings}>
+                        {images.map((image, index) => (
+                            <div key={index} className={`bg-dark carousel-item ${classNameImages}`} onClick={() => handleImageClick(index)}>
+                                <img src={image} alt={`slide${index}`} />
+                            </div>
+                        ))}
+                    </Slider>
 
-            <Modal size={"lg"} show={showModal} onHide={handleCloseModal} centered>
-                <Modal.Header className="d-flex bg-dark p-0" style={{ position: 'relative' }}>
-                    <Button variant="link" onClick={handleCloseModal} style={{ position: 'absolute', top: '0px', right: '-40px', zIndex: 1 }}>
-                        <span aria-hidden="true" style={{ color: 'white', fontSize: '2.5rem' }}>&times;</span>
-                    </Button>
-                    <img src={selectedImage} alt="Selected" style={{ width: '100%', height: 'auto' }} />
-                </Modal.Header>
-            </Modal>
-
+                    <Modal size={"lg"} show={showModal} onHide={handleCloseModal} centered>
+                        <Modal.Header className="d-flex bg-dark p-0" style={{ position: 'relative' }}>
+                            <Button variant="link" onClick={handleCloseModal} style={{ position: 'absolute', top: '0px', right: '-40px', zIndex: 1 }}>
+                                <span aria-hidden="true" style={{ color: 'white', fontSize: '2.5rem' }}>&times;</span>
+                            </Button>
+                            <img src={selectedImage} alt="Selected" style={{ width: '100%', height: 'auto' }} />
+                        </Modal.Header>
+                    </Modal>
+                </>
+            )}
         </>
     );
 };
--- src/systems/UploadSystem/articleData/articleData.js
+++ src/systems/UploadSystem/articleData/articleData.js
@@ -1,117 +1,128 @@
-import {auth, database, functions, storage} from "../../../firebase";
+import { auth, database, functions, storage } from "../../../firebase";
+import { deleteObject, getDownloadURL, getMetadata, ref } from "firebase/storage";
+import { get, onValue, ref as databaseRef } from "firebase/database";
+import { httpsCallable } from "firebase/functions";
+import { getIdTokenResult, signOut } from "firebase/auth";
 
-
-import {deleteObject, getDownloadURL, getMetadata, ref} from "firebase/storage";
-import {get, onValue, ref as databaseRef} from "firebase/database";
-import {httpsCallable} from "firebase/functions";
-import {getIdTokenResult, signOut} from "firebase/auth";
-
-export const setClaims = async (id, isEmail, claim)=> {
+// Set custom claims for a user
+export const setClaims = async (id, isEmail, claim) => {
     const adminFunction = httpsCallable(functions, 'setCustomClaim');
     try {
-        return await adminFunction({id: id, isEmail: isEmail, claim: claim})
-    }catch (e) {
+        return await adminFunction({ id, isEmail, claim });
+    } catch (e) {
         throw e;
     }
-}
-
-export const setAuthor = async (id, isEmail, isAuthor)=>{
-    const claim = {admin: isAuthor};
-    try {
-        return await setClaims(id, isEmail, claim);
-    }catch (e) {
-        throw e;
-    }
-}
-
-// disableUser
-
-export const disableUser = async (id, isEmail, disabled)=> {
-    const adminFunction = httpsCallable(functions, 'disableUser');
-    try {
-        return await adminFunction({id: id, isEmail: isEmail, disabled:disabled})
-    }catch (e) {
-        throw e;
-    }
-}
-
-
-const fetchAllFiles = (setArticlesByCategory, setLoading, pageToken) => {
-    const articlesRef = databaseRef(database, 'articlesList');
-    return onValue(articlesRef, async (snapshot)=> {
-        try {
-            if (snapshot.exists()) {
-                const data = snapshot.val();
-                const folders = Object.keys(data);
-                const articles = {};
-
-                // Fetch articles for each folder
-                for (const folder of folders) {
-                    const folderRef = databaseRef(database, `articlesList/${folder}`);
-                    const folderSnapshot = await get(folderRef);
-
-                    if (folderSnapshot.exists()) {
-                        const articlesByCategory = [];
-                        const categoryData = folderSnapshot.val();
-
-                        // Iterate over articles in the category
-                        Object.keys(categoryData).forEach(articleKey => {
-                            const articleCategory = categoryData[articleKey];
-                            Object.keys(articleCategory).forEach((articleTitle) => {
-                                const temp = articleCategory[articleTitle];
-
-                                temp.name = articleTitle;
-                                temp.category = articleKey || 'uncategorized';
-                                temp.folder = folder;
-
-                                articlesByCategory.push(temp);
-                            })
-                        });
-                        articles[folder] = articlesByCategory;
-                    }
-                }
-                setArticlesByCategory(articles);
-            }
-
-            setLoading(false);
-        } catch (error) {
-            console.error('Error fetching files:', error);
-            setLoading(false);
-        }
-    });
 };
 
-export async function fetchFiles(setFiles, setError, setAlreadyPublishedArticles, setAlreadyPublishedError, setEarlyReleasedArticles, setEarlyReleasesError, setLoading){
-    return fetchAllFiles(setFiles, setAlreadyPublishedError, setLoading, null)
-}
-
-
-
-export const getFirebaseStorageUrlFull = async (fileName, shouldBeFull) => {
-
-    if(shouldBeFull){
-        return await getDownloadURL(ref(storage, fileName));
-    }else{
-        let shouldResize = await isAlmostRectangle(ref(storage, fileName));
-        const imageRef = ref(storage, changeAnalysis(fileName, "800x800", "800x600", shouldResize.result));
-        try{
-            await getMetadata(imageRef);
-            return await getDownloadURL(imageRef);
-        }catch (e) {
-            return await getDownloadURL(ref(storage, fileName));
-        }
+// Set author claim
+export const setAuthor = async (id, isEmail, isAuthor) => {
+    const claim = { admin: isAuthor };
+    try {
+        return await setClaims(id, isEmail, claim);
+    } catch (e) {
+        throw e;
     }
+};
+
+// Disable a user
+export const disableUser = async (id, isEmail, disabled) => {
+    const adminFunction = httpsCallable(functions, 'disableUser');
+    try {
+        return await adminFunction({ id, isEmail, disabled });
+    } catch (e) {
+        throw e;
+    }
+};
+
+// Fetch all files and their articles from Firebase
+const fetchAllFiles = async (setArticlesByCategory, setLoading) => {
+    const articlesRef = databaseRef(database, 'articlesList');
+    try {
+        const snapshot = await get(articlesRef);
+        if (!snapshot.exists()) {
+            setLoading(false);
+            return;
+        }
+
+        const data = snapshot.val();
+        const folders = Object.keys(data);
+
+        const fetchFolderPromises = folders.map(async (folder) => {
+            const folderRef = databaseRef(database, `articlesList/${folder}`);
+            const folderSnapshot = await get(folderRef);
+
+            if (folderSnapshot.exists()) {
+                const articlesByCategory = [];
+                const categoryData = folderSnapshot.val();
+
+                Object.keys(categoryData).forEach(articleKey => {
+                    const articleCategory = categoryData[articleKey];
+                    Object.keys(articleCategory).forEach((articleTitle) => {
+                        const temp = articleCategory[articleTitle];
+                        temp.name = articleTitle;
+                        temp.category = articleKey || 'uncategorized';
+                        temp.folder = folder;
+                        articlesByCategory.push(temp);
+                    });
+                });
+
+                return { [folder]: articlesByCategory };
+            }
+            return null;
+        });
+
+        const foldersData = await Promise.all(fetchFolderPromises);
+        const articles = foldersData.reduce((acc, folderData) => {
+            if (folderData) {
+                return { ...acc, ...folderData };
+            }
+            return acc;
+        }, {});
+
+        setArticlesByCategory(articles);
+        setLoading(false);
+    } catch (error) {
+        console.error('Error fetching files:', error);
+        setLoading(false);
+    }
+};
+
+// Fetch files function
+export async function fetchFiles(setFiles, setError, setAlreadyPublishedArticles, setAlreadyPublishedError, setEarlyReleasedArticles, setEarlyReleasesError, setLoading) {
+    return fetchAllFiles(setFiles, setLoading);
 }
 
-export function getRef(imageUrl, isFolderIncluded){
+// Get Firebase Storage URL with optional resizing
+export const getFirebaseStorageUrlFull = async (fileName, shouldBeFull) => {
+    try {
+        if (shouldBeFull) {
+            return await getDownloadURL(ref(storage, fileName));
+        } else {
+            const metadata = await getMetadata(ref(storage, fileName)).catch(() => null);
+            if (metadata) {
+                const shouldResize = await isAlmostRectangle(ref(storage, fileName));
+                const imageRef = ref(storage, changeAnalysis(fileName, "800x800", "800x600", shouldResize.result));
+                return await getDownloadURL(imageRef);
+            } else {
+                return await getDownloadURL(ref(storage, fileName));
+            }
+        }
+    } catch (error) {
+        console.error('Error fetching image URL:', error);
+        throw error;
+    }
+};
+
+// Get file reference from URL
+export function getRef(imageUrl, isFolderIncluded) {
     const imagePath = imageUrl.split("/");
     const fileName = imagePath.pop();
-    const folder = isFolderIncluded?imagePath.join("/"):"images";
-
+    const folder = isFolderIncluded ? imagePath.join("/") : "images";
     return `${folder}/${fileName}`;
 }
 
-export const getFirebaseStorageUrlFromPath=async (imageUrl, isFolderIncluded)=>{
+// Get Firebase Storage URL from path with optional resizing
+export const getFirebaseStorageUrlFromPath = async (imageUrl, isFolderIncluded) => {
     const shouldResize = await isAlmostRectangle(ref(storage, imageUrl));
     const fileName = getRef(imageUrl, isFolderIncluded);
     const storageRef = ref(storage, changeAnalysis(fileName, "800x800", "800x600", shouldResize.result));
@@ -123,29 +134,31 @@ export const getFirebaseStorageUrlFromPath=async (imageUrl, isFolderIncluded)=>{
     }
 }
 
+// Get Firebase Storage URL with metadata storage
 export const getFirebaseStorageUrl = async (imageUrl, setShouldStoreMetadata, setImageStorageRef) => {
     const fileName = getRef(imageUrl, false);
     const shouldResize = await isAlmostRectangle(ref(storage, fileName));
     const storageRef = ref(storage, `${changeAnalysis(fileName, "800x800", "800x600", shouldResize.result)}`);
-    if(!!setShouldStoreMetadata) {
-        setShouldStoreMetadata(!shouldResize.areMetadataFound)
+    if (setShouldStoreMetadata) {
+        setShouldStoreMetadata(!shouldResize.areMetadataFound);
     }
-    if(!!setImageStorageRef) {
+    if (setImageStorageRef) {
         setImageStorageRef(storageRef);
     }
     try {
         return await getDownloadURL(storageRef);
     } catch (e) {
-        console.log(e);
+        console.error(e);
     }
 };
 
-export function deleteImage(imageName){
-    return Promise.all(getAllImageNames(imageName).map(async (imageName)=> {
+// Delete image and related files
+export function deleteImage(imageName) {
+    return Promise.all(getAllImageNames(imageName).map(async (imageName) => {
         const storageRef = ref(storage, imageName);
 
         try {
-            deleteObject(storageRef).then().catch(console.log);
+            await deleteObject(storageRef);
             console.log(`Image ${imageName} deleted successfully from storage.`);
         } catch (e) {
             console.error(`Error deleting image ${imageName}:`, e);
@@ -153,53 +166,44 @@ export function deleteImage(imageName){
     }));
 }
 
-export function getAllImageNames(imageName){
+// Get all image names with different resolutions
+export function getAllImageNames(imageName) {
     const dotIndex = imageName.lastIndexOf('.');
     const extensions = ["600x300", "600x600", "800x400", "800x600", "800x800"];
-    if (dotIndex === -1) {
-        return extensions.map((extension)=>`${imageName}_${extension}`);
-    }
-    const name = imageName.substring(0, dotIndex);
-    const imageExtension = imageName.substring(dotIndex);
-    return extensions.map((extension)=>`${name}_${extension}${imageExtension}`);
+    const name = dotIndex === -1 ? imageName : imageName.substring(0, dotIndex);
+    const imageExtension = dotIndex === -1 ? '' : imageName.substring(dotIndex);
+    return extensions.map((extension) => `${name}_${extension}${imageExtension}`);
 }
 
+// Change analysis based on dimensions
 function changeAnalysis(fileName, analysis_true, analysis_false, change_analysis) {
-    // Find the position of the last dot, which indicates the start of the extension
     const dotIndex = fileName.lastIndexOf('.');
-
-    // If there's no dot, return the filename with the suffix appended
     if (dotIndex === -1) {
         return `${fileName}_${change_analysis ? analysis_true : analysis_false}`;
     }
-
-    // Extract the name and extension parts
     const name = fileName.substring(0, dotIndex);
     const extension = fileName.substring(dotIndex);
-
-    // Construct the new filename
     return `${name}_${change_analysis ? analysis_true : analysis_false}${extension}`;
 }
 
+// Check if the image is "almost rectangular"
 const isAlmostRectangle = async (storageRef) => {
     try {
         const metadata = await getMetadata(storageRef);
         const width = parseInt(metadata.customMetadata.width, 10);
         const height = parseInt(metadata.customMetadata.height, 10);
 
-        // Define the tolerance for "almost rectangular" (e.g., within 10% difference)
         const tolerance = 0.5;
         const aspectRatio = width / height;
-        const result = Math.abs(aspectRatio - 1) <= tolerance
+        const result = Math.abs(aspectRatio - 1) <= tolerance;
 
-        return {result: result, areMetadataFound: true};
+        return { result, areMetadataFound: true };
     } catch (e) {
-        return {result: false, areMetadataFound: false};
+        return { result: false, areMetadataFound: false };
     }
 };
 
-
-
+// Get image dimensions from a file
 export const getImageDimensions = (file) => {
     return new Promise((resolve, reject) => {
         const img = new Image();
@@ -211,11 +215,12 @@ export const getImageDimensions = (file) => {
     });
 };
 
-export function handleAuthorTest(user, setCurrentUser, navigate){
+// Handle author test and navigation
+export function handleAuthorTest(user, setCurrentUser, navigate) {
     if (user) {
         getIdTokenResult(user).then((idTokenResult) => {
             if (idTokenResult.claims && idTokenResult.claims.admin) {
-                console.log("the user is an admin");
+                console.log("The user is an admin");
             } else {
                 signOut(auth).then(() => {
                     console.log("Trying to login again");
@@ -231,7 +236,8 @@ export function handleAuthorTest(user, setCurrentUser, navigate){
     }
 }
 
-export const categories =  [
+// List of categories
+export const categories = [
     "Top News",
     "General News",
     "Interviews",
@@ -240,32 +246,23 @@ export const categories =  [
     "Latest Reviews(GRE)",
     "Latest Reviews(IT)",
     "Legends"
-]
+];
 
-export const setUids= async (userList, leaderList, setAdminUids, setLeadersUids)=>{
+// Set user IDs based on emails
+export const setUids = async (userList, leaderList, setAdminUids, setLeadersUids) => {
     const authorsRef = databaseRef(database, 'authors');
     const authorsSnapshot = await get(authorsRef);
     const authorsData = authorsSnapshot.val();
 
-    const adminUids = userList.map(email => {
+    const findUids = (list) => list.map(email => {
         for (const uid in authorsData) {
             if (authorsData[uid].email === email) {
                 return uid;
             }
         }
-        return null; // Return null if no match is found
+        return null;
     });
 
-    setAdminUids(adminUids.filter(uid => uid !== null));
-
-    const leaderUids = leaderList.map(email => {
-        for (const uid in authorsData) {
-            if (authorsData[uid].email === email) {
-                return uid;
-            }
-        }
-        return null; // Return null if no match is found
-    });
-
-    setLeadersUids(leaderUids.filter(uid => uid !== null));
-}
+    setAdminUids(findUids(userList).filter(uid => uid !== null));
+    setLeadersUids(findUids(leaderList).filter(uid => uid !== null));
+};
